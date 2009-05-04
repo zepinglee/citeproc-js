8d06c89e6848dd9d72b5eab6c7594fcffbf40c6a
Frank Bennett <biercenator@gmail.com>
2009-05-05 07:51:20 +0900

    Flipflopper class built and tested.

--- src/util_flipflop.js
+++ src/util_flipflop.js
@@ -1,5 +1,45 @@
 dojo.provide("csl.util_flipflop");
 
-CSL.Util.FlipFlop = function(){
-	
+CSL.Util.FlipFlopper = function(){
+	this.flipflops = [];
+};
+
+CSL.Util.FlipFlopper.prototype.register = function(start, end, func, opt){
+	var flipflop = {
+		"start": start,
+		"end": end,
+		"func": func,
+		"opt": opt
+	};
+	this.flipflops.push(flipflop);
+};
+
+CSL.Util.FlipFlopper.prototype.split = function(idx,str){
+	var spec = this.flipflops[idx];
+	var lst1 = str.split(spec["start"]);
+	if (spec["start"] == spec["end"]){
+		if (lst1.length && (lst1.length % 2) == 0){
+			var buf = lst1.pop();
+			lst1[(lst1.length-1)] += spec["start"];
+			lst1[(lst1.length-1)] += buf;
+		}
+		var lst2 = lst1.slice();
+	} else {
+		var lst2 = [lst1[0]];
+		var do_me = true;
+		for (var i=1; i < lst1.length; i++){
+			var sublst = lst1[i].split(spec["end"]);
+			if (sublst.length == 1){
+				var buf = lst2.pop();
+				lst2.push( buf + spec["start"] + lst1.slice(i).join(spec["start"]) );
+				break;
+			}
+			sublst[1] = sublst.slice(1).join(spec["end"]);
+			for (var j=(sublst.length-1); j > 1; j--){
+				sublst.pop();
+			}
+			lst2 = lst2.concat(sublst);
+		}
+	}
+	return lst2;
 }
--- tests/test_flipflop.js
+++ tests/test_flipflop.js
@@ -1,15 +1,125 @@
 dojo.provide("tests.test_flipflop");
 
 doh.register("tests.flipflop", [
-	function testRegister() {
+//		var res = ff.split( 0, "One two [Y] three [X] four five [X]" );
+	function testCompleteNonsenseFailsGracefully(){
+		var dummyfunc = function(){};
+		var ff = new CSL.Util.FlipFlopper();
+		ff.register( "[X]", "[Y]", dummyfunc, []);
+		var res = ff.split( 0, "One two [Y] three [X] four five [X]" );
+		doh.assertEqual( 1, res.length );
+		doh.assertEqual( "One two [Y] three [X] four five [X]", res[0] );
+	},
+	function testStartAndEndMarkersIdenticalOk(){
+		var dummyfunc = function(){};
+		var ff = new CSL.Util.FlipFlopper();
+		ff.register( "*", "*", dummyfunc, []);
+		var res = ff.split( 0, "One two *three* four five six" );
+		doh.assertEqual( 3, res.length );
+		doh.assertEqual( "One two ", res[0] );
+		doh.assertEqual( "three", res[1] );
+		doh.assertEqual( " four five six", res[2] );
+	},
+	function testMismatchIdenticalStartAndEndMarkersFailsGracefully(){
+		var dummyfunc = function(){};
+		var ff = new CSL.Util.FlipFlopper();
+		ff.register( "*", "*", dummyfunc, []);
+		var res = ff.split( 0, "One two *three* four *five six" );
+		doh.assertEqual( 3, res.length );
+		doh.assertEqual( "One two ", res[0] );
+		doh.assertEqual( "three", res[1] );
+		doh.assertEqual( " four *five six", res[2] );
+	},
+	function testMismatchExtraStartTagBreaksParseSubsequently(){
+		var dummyfunc = function(){};
+		var ff = new CSL.Util.FlipFlopper();
+		ff.register( "[X]", "[Y]", dummyfunc, []);
+		var res = ff.split( 0, "One [X] two [Y] three [X] four [X] five [Y] six" );
+		doh.assertEqual( 3, res.length );
+		doh.assertEqual( "One ", res[0] );
+		doh.assertEqual( " two ", res[1] );
+		doh.assertEqual( " three [X] four [X] five [Y] six", res[2] );
+	},
+	function testMismatchExtraStartTagBreaksParse(){
+		var dummyfunc = function(){};
+		var ff = new CSL.Util.FlipFlopper();
+		ff.register( "[X]", "[Y]", dummyfunc, []);
+		var res = ff.split( 0, "One two [X] three [X] four five [Y] six" );
+		doh.assertEqual( 1, res.length );
+		doh.assertEqual( "One two [X] three [X] four five [Y] six", res[0] );
+	},
+	function testMismatchDoubleEndTagsIgnored(){
+		var dummyfunc = function(){};
+		var ff = new CSL.Util.FlipFlopper();
+		ff.register( "[X]", "[Y]", dummyfunc, []);
+		var res = ff.split( 0, "One two [X] three [Y] four five [Y] six [Y]" );
+		doh.assertEqual( 3, res.length );
+		doh.assertEqual( "One two ", res[0] );
+		doh.assertEqual( " three ", res[1] );
+		doh.assertEqual( " four five [Y] six [Y]", res[2] );
+	},
+	function testInstantiation() {
 		try {
 			var dummyfunc = function(){};
-			var ff = new CSL.Util.FlipFlop( ['X',dummyfunc], ['X',dummyfunc] );
+			var ff = new CSL.Util.FlipFlopper();
 			var ret = "Success";
 		} catch (e) {
 			var ret = "Oops: "+e;
 		}
 
 		doh.assertEqual( "Success", ret );
+	},
+	function testRegister() {
+		var dummyfunc = function(){};
+		var ff = new CSL.Util.FlipFlopper();
+		ff.register( "X", "Y", dummyfunc, ["preserve"]);
+		try {
+			var res = typeof ff.flipflops[0].func;
+		} catch (e) {
+			var res = "Oops: "+e;
+		}
+		doh.assertEqual( "function", res );
+	},
+	function testSplitSimpleOk(){
+		var dummyfunc = function(){};
+		var ff = new CSL.Util.FlipFlopper();
+		ff.register( "[X]", "[Y]", dummyfunc, []);
+		var res = ff.split( 0, "One two three [X] four five [Y] six" );
+		doh.assertEqual( 3, res.length );
+		doh.assertEqual( "One two three ", res[0] );
+		doh.assertEqual( " four five ", res[1] );
+		doh.assertEqual( " six", res[2] );
+	},
+	function testSplitStartAtStartOk(){
+		var dummyfunc = function(){};
+		var ff = new CSL.Util.FlipFlopper();
+		ff.register( "[X]", "[Y]", dummyfunc, []);
+		var res = ff.split( 0, "[X] four five [Y] six" );
+		doh.assertEqual( 3, res.length );
+		doh.assertEqual( "", res[0] );
+		doh.assertEqual( " four five ", res[1] );
+		doh.assertEqual( " six", res[2] );
+	},
+	function testSplitEndAtEndOk(){
+		var dummyfunc = function(){};
+		var ff = new CSL.Util.FlipFlopper();
+		ff.register( "[X]", "[Y]", dummyfunc, []);
+		var res = ff.split( 0, "One two three [X] four five [Y]" );
+		doh.assertEqual( 3, res.length );
+		doh.assertEqual( "One two three ", res[0] );
+		doh.assertEqual( " four five ", res[1] );
+		doh.assertEqual( "", res[2] );
+	},
+	function testSplitMultipleOk(){
+		var dummyfunc = function(){};
+		var ff = new CSL.Util.FlipFlopper();
+		ff.register( "[X]", "[Y]", dummyfunc, []);
+		var res = ff.split( 0, "One [X] two [Y] three [X] four five [Y]" );
+		doh.assertEqual( 5, res.length );
+		doh.assertEqual( "One ", res[0] );
+		doh.assertEqual( " two ", res[1] );
+		doh.assertEqual( " three ", res[2] );
+		doh.assertEqual( " four five ", res[3] );
+		doh.assertEqual( "", res[4] );
 	},
 ]);
