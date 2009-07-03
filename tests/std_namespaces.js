7cde64efa787cdc437939370b888fb2b89109a9b
Frank Bennett <biercenator@gmail.com>
2009-07-03 10:31:35 +0900

    Round out namespace tests, to demonstrate the failure of fragments, and the apparently erroneous successful rendering of full styles without a namespace declaration.

--- tests/std_namespaces.js
+++ tests/std_namespaces.js
@@ -6,7 +6,27 @@ doh.register("tests.std_namespaces", [
         doh.assertEqual(test.result, test.run());
     },
     function(){
+        var test = new StdRhinoTest("namespaces_Nada3");
+        doh.assertEqual(test.result, test.run());
+    },
+    function(){
+        var test = new StdRhinoTest("namespaces_Nada4");
+        doh.assertEqual(test.result, test.run());
+    },
+    function(){
         var test = new StdRhinoTest("namespaces_Nada");
+        doh.assertEqual(test.result, test.run());
+    },
+    function(){
+        var test = new StdRhinoTest("namespaces_NonNada2");
+        doh.assertEqual(test.result, test.run());
+    },
+    function(){
+        var test = new StdRhinoTest("namespaces_NonNada3");
+        doh.assertEqual(test.result, test.run());
+    },
+    function(){
+        var test = new StdRhinoTest("namespaces_NonNada4");
         doh.assertEqual(test.result, test.run());
     },
     function(){
