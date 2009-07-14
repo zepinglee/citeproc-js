fde1e5cfb267179aefff5406e6d2b8c6c066004f
Frank Bennett <biercenator@gmail.com>
2009-07-14 11:20:09 +0900

    Test of nonexistent date variable on key element in sort context.

--- tests/std_date.js
+++ tests/std_date.js
@@ -6,6 +6,10 @@ doh.register("tests.std_date", [
         doh.assertEqual(test.result, test.run());
     },
     function(){
+        var test = new StdRhinoTest("date_IgnoreNonexistentSort");
+        doh.assertEqual(test.result, test.run());
+    },
+    function(){
         var test = new StdRhinoTest("date_KeyVariable");
         doh.assertEqual(test.result, test.run());
     },
