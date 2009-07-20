f792370a5f4254fa03b1d48053920cab997cda5e
Frank Bennett <biercenator@gmail.com>
2009-07-20 22:50:59 +0900

    Delete-by-item-id function for CSL.Factory.Registry.NameReg.  Testing will surely turn up many coding errors and design mistakes, but for now, it looks great.

--- src/namereg.js
+++ src/namereg.js
@@ -4,9 +4,7 @@ dojo.provide("csl.namereg");
  * The idea here will be to store names in a nested hierarchy
  * of declining ambiguity (surname, initial, given name), with
  * a query mechanism to return the correct disambig value for
- * the surname.  Each time a name is added or deleted, the query
- * mechanism is run, and the result is stored in names_inf for
- * rapid delivery via an external query method.
+ * the surname.
  *
  * What should happen is that names are filed in a
  * names registry when they are used.  so state.registry.names=(new something).
@@ -14,24 +12,8 @@ dojo.provide("csl.namereg");
  * lowest acceptable value for the current configuration state.
  * var lev = state.registry.names(state,name);, like.
  *
- * This could be extended later to a simplification of all names
- * disambiguation, by returning both the minimum AND maximum
- * name config values from this method.  That would put all the
- * initialize-with etc jiggery-pokery in one place, and the
- * disambiguation machinery would not need to be cluttered up with
- * it; you would just increment and decrement name config values
- * within the range.  But that's for later.  For now, we just
- * set a floor value and use the existing machinery.
- *
- * No, the extension might as well happen now.  Setting the ceiling
- * through this function will bring all of the names parameters into
- * this space, where they can be easily seen and controlled.  Very
- * hard adjustments in the disambiguation machinery, but small steps,
- * man, small steps.
- *
  */
 CSL.Factory.Registry.NameReg = function(state){
-	//this.state = state;
 	this.namereg = new Object();
 	this.nameind = new Object();
 	//
@@ -133,66 +115,32 @@ CSL.Factory.Registry.NameReg = function(state){
 			pkey = key[0];
 			ikey = key[1];
 			skey = key[2];
-			delete this.namereg[pkey].ikey[ikey].skey[skey].items[item];
-			delete this.namereg[pkey].ikey[ikey].items[item];
-			delete this.namereg[pkey].items[item];
+			var pos = this.namereg[pkey].items.indexOf(item);
+			var items = this.namereg[pkey].items;
+			if (skey){
+				pos = this.namereg[pkey].ikey[ikey].skey[skey].items.indexOf(item);
+				items = this.namereg[pkey].ikey[ikey].skey[skey].items;
+				this.namereg[pkey].ikey[ikey].skey[skey].items = items.slice(0,pos).concat(items.slice([pos+1],items.length));
+				if (this.namereg[pkey].ikey[ikey].skey[skey].items.length == 0){
+					delete this.namereg[pkey].ikey[ikey].skey[skey];
+					this.namereg[pkey].ikey[ikey].count += -1;
+				};
+			};
+			if (ikey){
+				pos = this.namereg[pkey].ikey[ikey].items.indexOf(item);
+				items = this.namereg[pkey].ikey[ikey].items;
+				this.namereg[pkey].ikey[ikey].items = items.slice(0,pos).concat(items.slice([pos+1],items.length));
+				if (this.namereg[pkey].ikey[ikey].items.length == 0){
+					delete this.namereg[pkey].ikey[ikey];
+					this.namereg[pkey].count += -1;
+				};
+			};
+			this.namereg[pkey].items = items.slice(0,pos).concat(items.slice([pos+1],items.length));
+			delete this.nameid[item];
 		};
 	};
-
 	//
-	// Oooooooh, this is hard.  The problem is where to call
-	// this method from.  It can't be called on all names in an
-	// Item, because that will be overaggressive (there might be
-	// only one name actually visible in printed output).
-	//
-	// If it's called per-name at render time, that should work
-	// okay (I think), but also need to be careful to call
-	// del method if the name is unwound during disambiguation
-	// (disambiguate-add-names).
-	//
-	// There's a problem, though.  Suppose [Alex] Smith, Book A (2000).
-	// Then suppose a later cite to [Arnold] Smith, Book B (1990).  The
-	// two sources are not in the same disambig set.  A separate tracking
-	// registry is needed for names, so that a request for re-rendering
-	// can be issued when the name form changes.
-	//
-	// And this needs to play nice with the existing machinery.
-	// Swell.  Just swell.
-	//
-	// Okay, maybe we can escape here.  skey is an array of
-	// item ids that contain the name.  we can tap into that
-	// later to request/trigger updates when the name changes.
-	//
-	// Uh-oh.  Need to cache the name param result for each name
-	// somewhere, though, so that we can tell when it's changed and
-	// when it hasn't ... except we can't, can we, since whether
-	// initials are used or not, and whether full or short form is
-	// used, is specific to the names tag (not global across the
-	// style, nor even across citation or bibliography).
-	//
-	// ... but disambiguation levels are set globally.  So I
-	// guess we can just blunder forward on this.  There are some
-	// option combinations that will break without fine-grained
-	// caching -- if one name element requests full form, that
-	// will force all instances of all names that it renders
-	// to full form -- but maybe that can be written off as
-	// CSL behavior, not an error in the program.
-	//
-	// okay, so this now works as an update method, and
-	// maintains a list of item ids requiring onward updates
-	// to keep them in sync with global names disambiguation
-	// status.
-	//
-	// alright, that works, but it breaks the add-names disambiguation
-	// routines.
-	//
-	// The add-names routines break because the base form of the
-	// name changes during processing, as further names are added.
-	// This causes the ambig key to shift around, with the result
-	// that names are not properly expanded, and adding of names
-	// fails.  Seems to be the problem, anyway.
-	//
-	// Let's try switching the order of processing.  Run ALL
+	// Run ALL
 	// renderings with disambiguate-add-givenname set to a value
 	// with the by-cite behaviour, and then set the names-based
 	// expanded form when the final makeCitationCluster rendering
@@ -200,7 +148,6 @@ CSL.Factory.Registry.NameReg = function(state){
 	// the state object in the execution wrappers that run the
 	// style.
 	//
-
 	var addname = function(item_id,nameobj,pos){
 		_set_keys(nameobj);
 		// pkey, ikey and skey should be stored in separate cascading objects.
