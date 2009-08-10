7b8d8406ff54e6b7e1372c161026501383267c38
Frank Bennett <biercenator@gmail.com>
2009-08-10 23:15:42 +0900

    Removing another weird namespace test, that can't be made to both clear validation and test the processor.

--- tests/std_namespaces.js
+++ tests/std_namespaces.js
@@ -2,10 +2,6 @@ dojo.provide("tests.std_namespaces");
 
 doh.register("tests.std_namespaces", [
     function(){
-        var test = new StdRhinoTest("namespaces_Nada4");
-        doh.assertEqual(test.result, test.run());
-    },
-    function(){
         var test = new StdRhinoTest("namespaces_NonNada3");
         doh.assertEqual(test.result, test.run());
     },
