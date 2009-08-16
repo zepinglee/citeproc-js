b70304c65a1ebdd41bb4ae56b032a82889e3dda9
Frank Bennett <biercenator@gmail.com>
2009-08-16 22:43:38 +0900

    Apply the Common Public Attribution License to the sources.

---
+++ ref/APPLY_LICENSE.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import re,os,sys
+
+REX = "(?ms)^^(/\*.*?\n\s*\*/)(.*)"
+
+m = re.match(REX, open("./src/csl.js").read())
+
+
+if m:
+    license = m.group(1)
+else:
+    print "Oops, no license in csl.js"
+    sys.exit()
+
+
+for path in ["./src", "./std", "./std/humans","./tests"]:
+    for file in os.listdir( path ):
+        if not file.endswith(".js") and not file.endswith(".txt"): continue
+        if file == "README.txt": continue
+        text = open("%s/%s" %(path,file)).read()
+        m = re.match(REX,text)
+        if m:
+            text = "%s%s" % (license, m.group(2))
+        else:
+            text = "%s\n%s" % (license, text)
+        open("%s/%s" %(path,file),"w+").write(text)
+                        
