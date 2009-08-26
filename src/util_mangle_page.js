a6ce385591d736533e6d348084b5255c4930ce45
Frank Bennett <biercenator@gmail.com>
2009-08-26 14:26:03 +0900

    Move working python test code for page range mangling into util_mangle_page.js

--- src/util_mangle_page.js
+++ src/util_mangle_page.js
@@ -53,3 +53,67 @@ CSL.Util.PageRangeMangler.prototype = function(){
 	};
 
 };
+
+/*
+ * #!/usr/bin/python
+ *
+ * from operator import idiv, mod
+ * def numStrDiff(begin, end):
+ *     b = list(str(begin))
+ *     e = list(str(end))
+ *     ret = e[:]
+ *     ret.reverse()
+ *     if len(b) == len(e):
+ *         for pos in range(0,len(b),1):
+ *             if b[pos] == e[pos]:
+ *                 ret.pop()
+ *             else:
+ *                 ret.reverse()
+ *                 break
+ *     return "".join(ret)
+ *
+ *
+ * def collapse(begin, end, RESULT):
+ *     print "Orig: %d to %d" % (begin, end)
+ *     if begin > 100 and mod(begin, 100) and idiv(begin, 100) == idiv(end, 100):
+ *         ret = (str(begin) + "-" + str(mod(end,100)))
+ *     elif begin >= 10000:
+ *         ret = (str(begin) + "-"  + numStrDiff(begin, end))
+ *     else:
+ *         ret = (str(begin) + "-" + str(end))
+ *     if ret == RESULT:
+ *         ok = " ... ok"
+ *     else:
+ *         ok = " ... FAIL (expected %s)" % RESULT
+ *     print "  Result: %s%s" %(ret,ok)
+ *     return ret
+ *
+ * collapse(101, 108, "101-8")
+ *
+ * collapse(3,10, "3-10")
+ * collapse(71,72, "71-72")
+ * collapse(96,117, "96-117")
+ * collapse(100,104, "100-104")
+ * collapse(600,613, "600-613")
+ * collapse(1100,1123, "1100-1123")
+ * collapse(107,108, "107-8")
+ * collapse(505,517, "505-17")
+ * collapse(1002,1006, "1002-6")
+ * collapse(321,325, "321-25")
+ * collapse(415,532, "415-532")
+ * collapse(1536,1538, "1536-38")
+ * collapse(11564,11568, "11564-68")
+ * collapse(11564,11578, "11564-78")
+ * collapse(13792,13803, "13792-803")
+ * #
+ * # The following two tests mail be failing or passing, depending on
+ * # the rule applied.  Do the following come from different editions
+ * # of CMS, or is the first of the two trying to fix a single algorithm
+ * # to cover page ranges and years?
+ * #
+ * # Compare http://www.aahn.org/guidelines.html
+ * # ... and http://msupress.msu.edu/journals/fch/admin/FCH%20Style%20Sheet%2015.pdf
+ * #
+ * collapse(1496,1504, "1496-1504")
+ * collapse(2787,2816, "2787-2816")
+ */
