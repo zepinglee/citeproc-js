84bcaefeb711134cd62e5018659aa7402ed7a367
Frank Bennett <biercenator@gmail.com>
2009-11-02 17:53:58 +0900

    Fix bug in test picked up by Rintze.  In the spirit of the Big Hammer Method, add the test that would have caught the error in my CSL for the test.

--- tests/std_name.js
+++ tests/std_name.js
@@ -148,6 +148,10 @@ doh.register("tests.std_name", [
         doh.assertEqual(test.result, test.run());
     },
     function(){
+        var test = new StdRhinoTest("name_StartAndJustOneMoreThanEtAlUseFirst");
+        doh.assertEqual(test.result, test.run());
+    },
+    function(){
         var test = new StdRhinoTest("name_StartAndLessThanMax");
         doh.assertEqual(test.result, test.run());
     },
