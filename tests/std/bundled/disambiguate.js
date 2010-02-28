01c6000a9de3e6f0e18f2c48b8b0413ac329083b
Frank Bennett <biercenator@gmail.com>
2010-02-28 23:53:42 +0900

    Fresh disambiguation tests.

--- tests/std/bundled/disambiguate.js
+++ tests/std/bundled/disambiguate.js
@@ -79,6 +79,10 @@ doh.register("std.disambiguate", [
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
+        var test = new StdRhinoTest("disambiguate_AllNamesWithInitialsGenerally");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
         var test = new StdRhinoTest("disambiguate_YearSuffixAndSort");
         doh.assertEqual(test.result, test.run());
     }, 
--- tests/std/bundled/nameattr.js
+++ tests/std/bundled/nameattr.js
@@ -203,6 +203,10 @@ doh.register("std.nameattr", [
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
+        var test = new StdRhinoTest("nameattr_AndOnNamesInBibliography");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
         var test = new StdRhinoTest("nameattr_NameAsSortOrderOnNamesInCitation");
         doh.assertEqual(test.result, test.run());
     }, 
@@ -248,10 +252,6 @@ doh.register("std.nameattr", [
     }, 
     function(){
         var test = new StdRhinoTest("nameattr_NameDelimiterOnCitationInBibliography");
-        doh.assertEqual(test.result, test.run());
-    }, 
-    function(){
-        var test = new StdRhinoTest("nameattr_AndOnNamesInBibliography");
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
