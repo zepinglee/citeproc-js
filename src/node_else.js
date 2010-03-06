071380199bab463ae9cd10fdc301d593925ab8ba
Frank Bennett <biercenator@gmail.com>
2010-03-07 07:04:09 +0900

    jslint fixes for node_else.js

--- src/node_else.js
+++ src/node_else.js
@@ -34,12 +34,12 @@
  */
 
 CSL.Node["else"] = {
-	build: function (state,target){
+	build: function (state, target) {
 		target.push(this);
 	},
-	configure: function (state,pos){
-		if (this.tokentype == CSL.START){
-			state.configure["fail"][(state.configure["fail"].length-1)] = pos;
+	configure: function (state, pos) {
+		if (this.tokentype === CSL.START) {
+			state.configure.fail[(state.configure.fail.length - 1)] = pos;
 		}
 	}
 };
