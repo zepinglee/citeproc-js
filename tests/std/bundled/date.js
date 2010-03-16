0978f0c92885f11b2a0e79c2600070afaa50c30b
Frank Bennett <biercenator@gmail.com>
2010-03-17 08:23:39 +0900

    Tests for localized date formats from Rintze Zelle.

--- tests/std/bundled/date.js
+++ tests/std/bundled/date.js
@@ -39,6 +39,14 @@ doh.register("std.date", [
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-de-AT");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-hu-HU");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
         var test = new StdRhinoTest("date_SortEmptyDatesBibliography");
         doh.assertEqual(test.result, test.run());
     }, 
@@ -55,7 +63,35 @@ doh.register("std.date", [
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-el-GR");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-en-US");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-de-DE");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-zh-TW");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-nl-NL");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
         var test = new StdRhinoTest("date_YearSuffixDelimiter");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-fr-FR");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-zh-CN");
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
@@ -75,11 +111,23 @@ doh.register("std.date", [
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-ko-KR");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-vi-VN");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
         var test = new StdRhinoTest("date_DateBC");
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
         var test = new StdRhinoTest("date_IgnoreNonexistentSort");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-es-ES");
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
@@ -99,11 +147,23 @@ doh.register("std.date", [
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-cs-CZ");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
         var test = new StdRhinoTest("date_DateNoDate");
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
         var test = new StdRhinoTest("date_SeasonSubstituteInGroup");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-et-EE");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_DateAD");
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
@@ -131,7 +191,15 @@ doh.register("std.date", [
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-pl-PL");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
         var test = new StdRhinoTest("date_LocalizedNumericYearRange");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-sl-SL");
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
@@ -139,7 +207,23 @@ doh.register("std.date", [
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-ar-AR");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-th-TH");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
         var test = new StdRhinoTest("date_TextFormFulldateDayRange");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-da-DK");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-bg-BG");
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
@@ -159,11 +243,35 @@ doh.register("std.date", [
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-uk-UA");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
         var test = new StdRhinoTest("date_TextFormMonthdateMonthRange");
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-sr-RS");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
         var test = new StdRhinoTest("date_LocalizedNumericYearMonthMissingMonth");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-ro-RO");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-ja-JP");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-de-CH");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-pt-BR");
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
@@ -179,6 +287,10 @@ doh.register("std.date", [
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-it-IT");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
         var test = new StdRhinoTest("date_LocalizedTextDefaultMissingDay");
         doh.assertEqual(test.result, test.run());
     }, 
@@ -187,11 +299,19 @@ doh.register("std.date", [
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-sv-SE");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
         var test = new StdRhinoTest("date_NonexistentSortReverseBibliography");
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
         var test = new StdRhinoTest("date_OtherAlone");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-mn-MN");
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
@@ -207,6 +327,14 @@ doh.register("std.date", [
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-sk-SK");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-ru-RU");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
         var test = new StdRhinoTest("date_LocalizedNumericYearWithAffixes");
         doh.assertEqual(test.result, test.run());
     }, 
@@ -215,11 +343,35 @@ doh.register("std.date", [
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
-        var test = new StdRhinoTest("date_DateAD");
+        var test = new StdRhinoTest("date_LocalizedDateFormats-nb-NO");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-is-IS");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-he-IL");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-tr-TR");
         doh.assertEqual(test.result, test.run());
     }, 
     function(){
         var test = new StdRhinoTest("date_LocalizedNumericYearMonth");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-ca-AD");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-pt-PT");
+        doh.assertEqual(test.result, test.run());
+    }, 
+    function(){
+        var test = new StdRhinoTest("date_LocalizedDateFormats-af-ZA");
         doh.assertEqual(test.result, test.run());
     }, 
 ]);
