216ea82e7681b44e6eb8c10096266d78ab8d2fe4
Frank Bennett <biercenator@gmail.com>
2010-04-28 06:06:50 +0900

    Updated demo page source, with Bluebook examples.

--- demo/loadabbrevs.js
+++ demo/loadabbrevs.js
@@ -1,13 +1,11 @@
-
-
 var abbreviations = {
 	"default": {
-		"journal":{
+		"container-title":{
 			"Pacific Rim Law & Policy Journal":"Pac. Rim L. & Pol\u2019y J.",
 			"\u65e5\u672c\u6559\u80b2\u5de5\u5b66\u4f1a\u8ad6\u6587\u8a8c":"\u65e5\u6559\u8ad6\u8a8c",
 			"Applied and Environmental Microbiology":"Applied and Environmental Microbiology"
 		},
-		"series":{
+		"collection-title":{
 			"International Rescue Wildlife Series":"I.R. Wildlife Series"
 		},
 		"authority":{
@@ -18,12 +16,12 @@ var abbreviations = {
 		}
 	},
 	"slightly_weird": {
-		"journal":{
+		"container-title":{
 			"Pacific Rim Law & Policy Journal":"PRL & PJ",
 			"\u65e5\u672c\u6559\u80b2\u5de5\u5b66\u4f1a\u8ad6\u6587\u8a8c":"\u65e5\u6559\u8ad6",
 			"Applied and Environmental Microbiology":"Appl'd. & Env'tal Microbio."
 		},
-		"series":{
+		"collection-title":{
 			"International Rescue Wildlife Series":"I.R. Wildlife Series"
 		},
 		"authority":{
--- demo/runcites.js
+++ demo/runcites.js
@@ -50,38 +50,212 @@ function pointedStickInnerHtml(elementid,content){
 	}
 }
 
-var citation = {
+var insertCites = function(cites) {
+	var pos, len, data, idx, cite, spannodes, nodes, node, id, ret;
+	nodes = [];
+	spannodes = document.getElementsByTagName("span");
+	for (pos = 0, len = spannodes.length; pos < len; pos += 1) {
+		node = spannodes.item(pos);
+		if (node.getAttribute("name") !== "citation") {
+			continue;
+		}
+		nodes.push(node);
+	}
+	for (pos = 0, len = cites.length; pos < len; pos += 1) {
+		data = cites[pos];
+		idx = data[0];
+		cite = data[1];
+		id = nodes[idx].id;
+		pointedStickInnerHtml(id,cite);
+	}
+}
+
+var citationSrc1 = {
 	"citationItems": [
-		{
-			id: "ITEM-1"
-		},
-		{
-			id: "ITEM-2"
-		},
-		{
-			id: "ITEM-3"
-		},
-		{
-			id: "ITEM-4"
-		},
-		{
-			id: "ITEM-5"
-		},
-		{
-			id: "ITEM-6"
-		},
-		{
-			id: "ITEM-10"
-		},
-		{
-			id: "ITEM-11"
-		},
-		{
-			id: "ITEM-12"
+        {
+			id: "ITEM-6",
+			label: "page",
+			locator: "223"
 		}
 	],
 	"properties": {
 		"noteIndex": 1
+	}
+};
+
+var citationSrc2 = {
+	"citationItems": [
+        {
+			id: "ITEM-6"
+		},
+        {
+			id: "ITEM-10"
+		},
+        {
+			id: "ITEM-11"
+		},
+        {
+			id: "ITEM-12"
+		},
+        {
+			id: "ITEM-6",
+			locator: "15",
+			prefix:"<i>but see</i>"
+		}
+	],
+	"properties": {
+		"noteIndex": 2
+	}
+};
+
+var citationSrc3 = {
+	"citationItems": [
+        {
+			id: "ITEM-13",
+			locator:"90",
+			label:"section"
+		},
+        {
+			id: "ITEM-14",
+			locator:"7",
+			label:"section"
+		},
+        {
+			id: "ITEM-15",
+			locator:"731-32",
+			label:"page"
+		},
+        {
+			id: "ITEM-16"
+		}
+	],
+	"properties": {
+		"noteIndex": 3
+	}
+};
+
+var citationSrc4 = {
+	"citationItems": [
+        {
+			id: "ITEM-2"
+		}
+	],
+	"properties": {
+		"noteIndex": 4
+	}
+};
+
+var citationSrc5 = {
+	"citationItems": [
+        {
+			id: "ITEM-2",
+			label:"page",
+			locator:"482"
+		},
+        {
+			id: "ITEM-13",
+			locator:"395",
+			label:"section"
+		},
+        {
+			id: "ITEM-1",
+			label:"page",
+			locator:"25"
+		},
+        {
+			id: "ITEM-1"
+		},
+        {
+			id: "ITEM-1",
+			label:"page",
+			locator:"112"
+		}
+	],
+	"properties": {
+		"noteIndex": 5
+	}
+};
+
+var citationSrc6 = {
+	"citationItems": [
+        {
+			id: "ITEM-16"
+		}
+	],
+	"properties": {
+		"noteIndex": 6
+	}
+};
+
+var citationSrc7 = {
+	"citationItems": [
+        {
+			id: "ITEM-17",
+			label:"paragraph",
+			locator:"6"
+		},
+        {
+			id: "ITEM-18",
+			label:"page",
+			locator:"983"
+		}
+	],
+	"properties": {
+		"noteIndex": 7
+	}
+};
+
+var citationSrc8 = {
+	"citationItems": [
+        {
+			id: "ITEM-19",
+			"suffix":"(appeal taken from Scot.)"
+		}
+	],
+	"properties": {
+		"noteIndex": 8
+	}
+};
+
+var citationSrc9 = {
+	"citationItems": [
+        {
+			id: "ITEM-20",
+			suffix:"(appeal taken from B.C.)"
+		}
+	],
+	"properties": {
+		"noteIndex": 9
+	}
+};
+
+var citationSrc10 = {
+	"citationItems": [
+        {
+			id: "ITEM-3",
+			prefix:"<b>En sinn Scholl beschéngt ass, da Mamm frësch blénken hun?</b> <i>See, e.g.</i>"
+		},
+        {
+			prefix:"<b>Der mä gutt Dach, eng onser bléit geplot mä.</b>  <i>See generally</i>",
+			id: "ITEM-3",
+			suffix:"<b>(Iwer Engel Milliounen nei fu, blëtzen néierens d'Gaassen rou do.)</b>"
+		}
+	],
+	"properties": {
+		"noteIndex": 10
+	}
+};
+
+var citationSrc11 = {
+	"citationItems": [
+        {
+			id: "ITEM-13",
+			label:"section",
+			locator:"1"
+		}
+	],
+	"properties": {
+		"noteIndex": 11
 	}
 };
 
@@ -98,23 +272,48 @@ var insert = function(){
 		pointedStickInnerHtml("chicago_author_date",output);
 	}
 
-	// Subsectioned
-	citeproc = new CSL.Engine(sys,chicago_fullnote_bibliography);
+	// Bluebook and subsectioned bib
+	citeproc = new CSL.Engine(sys,bluebook_demo);
 	citeproc.setAbbreviations("default");
-	citeproc.updateItems(["ITEM-1", "ITEM-2", "ITEM-3", "ITEM-4", "ITEM-5", "ITEM-6", "ITEM-10","ITEM-11","ITEM-12"]);
-	citeproc.appendCitationCluster(citation);
+	var citation1 = citeproc.appendCitationCluster(citationSrc1);
+	insertCites(citation1);
+	var citation2 = citeproc.appendCitationCluster(citationSrc2);
+	insertCites(citation2);
+	var citation3 = citeproc.appendCitationCluster(citationSrc3);
+	insertCites(citation3);
+	var citation4 = citeproc.appendCitationCluster(citationSrc4);
+	insertCites(citation4);
+	var citation5 = citeproc.appendCitationCluster(citationSrc5);
+	insertCites(citation5);
+	var citation6 = citeproc.appendCitationCluster(citationSrc6);
+	insertCites(citation6);
+	var citation7 = citeproc.appendCitationCluster(citationSrc7);
+	insertCites(citation7);
+	var citation8 = citeproc.appendCitationCluster(citationSrc8);
+	insertCites(citation8);
+	var citation9 = citeproc.appendCitationCluster(citationSrc9);
+	insertCites(citation9);
+	var citation10 = citeproc.appendCitationCluster(citationSrc10);
+	insertCites(citation10);
+	var citation11 = citeproc.appendCitationCluster(citationSrc11);
+	insertCites(citation11);
+
 	var cases = {
-		"select" : [
+		"include" : [
 			{
 				"field" : "type",
 				"value" : "legal_case"
+			},
+			{
+				"field" : "type",
+				"value" : "legislation"
 			}
 		]
 	};
 	output = citeproc.makeBibliography(cases);
 	if (output && output.length && output[1].length){
 		output = output[0].bibstart + output[1].join("") + output[0].bibend;
-		pointedStickInnerHtml("chicago_fullnote_bibliography_cases",output);
+		pointedStickInnerHtml("bluebook_demo_legal_stuff",output);
 	}
 	var books = {
 		"select" : [
@@ -127,7 +326,7 @@ var insert = function(){
 	output = citeproc.makeBibliography(books);
 	if (output && output.length && output[1].length){
 		output = output[0].bibstart + output[1].join("") + output[0].bibend;
-		pointedStickInnerHtml("chicago_fullnote_bibliography_books",output);
+		pointedStickInnerHtml("bluebook_demo_books",output);
 	}
 	var articles = {
 		"exclude" : [
@@ -138,13 +337,17 @@ var insert = function(){
 			{
 				"field" : "type",
 				"value" : "legal_case"
+			},
+			{
+				"field" : "type",
+				"value" : "legislation"
 			}
 		]
 	};
 	output = citeproc.makeBibliography(articles);
 	if (output && output.length && output[1].length){
 		output = output[0].bibstart + output[1].join("") + output[0].bibend;
-		pointedStickInnerHtml("chicago_fullnote_bibliography_articles",output);
+		pointedStickInnerHtml("bluebook_demo_articles",output);
 	}
 
 	// Listing
@@ -176,4 +379,5 @@ var insert = function(){
 		output = output[0].bibstart + output[1].join("") + output[0].bibend;
 		pointedStickInnerHtml("chicago_fullnote_bibliography2",output);
 	}
+
 };
