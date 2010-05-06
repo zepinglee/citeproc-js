ac76dde1997891e7cc2f8583a02e91f6452ad000
Frank Bennett <biercenator@gmail.com>
2010-05-07 08:14:56 +0900

    Adjust test of year-suffix string generator to require a correct result.

--- tests/citeproc-js/suffixator.js
+++ tests/citeproc-js/suffixator.js
@@ -42,14 +42,17 @@ doh.register("citeproc_js.suffixator", [
 		var list = "a,b,c,d";
 		var suffixator = new CSL.Util.Suffixator(list);
 
+		var res = suffixator.get_suffixes(8,list);
+		doh.assertEqual("ba", res[(res.length-1)]);
+
 		var res = suffixator.get_suffixes(34,list);
 		doh.assertEqual("adc", res[(res.length-1)]);
 
 		var res = suffixator.get_suffixes(350,list);
-		doh.assertEqual("acbbd", res[(res.length-1)]);
+		doh.assertEqual("aaacc", res[(res.length-1)]);
 
 		var res = suffixator.get_suffixes(351,list);
-		doh.assertEqual("acbca", res[(res.length-1)]);
+		doh.assertEqual("aaacd", res[(res.length-1)]);
 	},
 	function testGetSuffix(){
 		var list = "a,b,c,d";
@@ -57,3 +60,4 @@ doh.register("citeproc_js.suffixator", [
 		doh.assertEqual("c",suffixator.format(2));
 	},
 ]);
+
