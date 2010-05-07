3906e961bddf9e81cdd1ab34a69a47415d97276b
Frank Bennett <biercenator@gmail.com>
2010-05-07 09:00:15 +0900

    Bundled code for version change (with correct version set in code).  Ready for 1.0.10 release.

--- citeproc.js
+++ citeproc.js
@@ -1265,7 +1265,7 @@ CSL.dateParser = function (txt) {
 };
 CSL.Engine = function (sys, style, lang, xmlmode) {
 	var attrs, langspec, localexml, locale;
-	this.processor_version = "1.0.9";
+	this.processor_version = "1.0.10";
 	this.csl_version = "1.0";
 	this.sys = sys;
 	this.sys.xml = new CSL.System.Xml.Parsing();
