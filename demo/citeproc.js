39fada7054a6228d3710539aa0851f7a8fc43e59
Frank Bennett <biercenator@gmail.com>
2010-05-22 21:28:07 +0900

    Bundled code reflecting recent changes.

--- demo/citeproc.js
+++ demo/citeproc.js
@@ -1237,7 +1237,7 @@ CSL.dateParser = function (txt) {
 };
 CSL.Engine = function (sys, style, lang, xmlmode) {
 	var attrs, langspec, localexml, locale;
-	this.processor_version = "1.0.12";
+	this.processor_version = "1.0.13";
 	this.csl_version = "1.0";
 	this.sys = sys;
 	this.sys.xml = new CSL.System.Xml.Parsing();
