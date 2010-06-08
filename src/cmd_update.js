8b68ccd24261ca02f64a821fe18a9101c8ded14e
Frank Bennett <biercenator@gmail.com>
2010-06-08 17:14:40 +0900

    Fix obvious memory leaks and thrashing in code of restoreProcessorState().

--- src/cmd_update.js
+++ src/cmd_update.js
@@ -56,8 +56,8 @@ CSL.Engine.prototype.restoreProcessorState = function (citations) {
 	// sorted as required by the current style.
 	citationList = [];
 	itemList = [];
-	sortedItems = [];
 	for (pos = 0, len = citations.length; pos < len; pos += 1) {
+		sortedItems = [];
 		for (ppos = 0, len = citations[pos].citationItems.length; ppos < llen; ppos += 1) {
 			item = citations[pos].citationItems[ppos];
 			Item = this.sys.retrieveItem(item.id);
@@ -70,14 +70,14 @@ CSL.Engine.prototype.restoreProcessorState = function (citations) {
 			sortedItems.sort(this.citation.srt.compareCompositeKeys);
 		}
 		citations[pos].sortedItems = sortedItems;
-		// Register Items
-		this.updateItems(itemList);
 		// Save citation data in registry
 		this.registry.citationreg.citationById[citations[pos].citationID] = citations[pos];
 		// Save off citationIDs and index positions, for use in
 		// final initialization.
 		citationList.push([citations[pos].citationID, citations[pos].properties.noteIndex]);
 	}
+	// Register Items
+	this.updateItems(itemList);
 	// Rendering one citation restores remainder of processor state.
 	if (citations && citations.length) {
 		this.processCitationCluster(citations[0], [], citationList.slice(1));
