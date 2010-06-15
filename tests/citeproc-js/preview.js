501aa3c557b8ee708b483c3bfd7c64252ba9b60e
Frank Bennett <biercenator@gmail.com>
2010-06-15 18:16:07 +0900

    Tests for previewCitationCluster().

--- tests/citeproc-js/preview.js
+++ tests/citeproc-js/preview.js
@@ -59,6 +59,17 @@ var mycsl = "<style>"
 	  + "</citation>"
 	+ "</style>";
 
+var mycsl2 = "<style>"
+	  + "<citation disambiguate-add-givenname=\"true\">"
+	  + "  <layout delimiter=\"; \" prefix=\"(\" suffix=\")\">"
+	  + "    <names variable=\"author\">"
+	  + "    <name form=\"short\" initialize-with=\". \"/>"
+	  + "    </names>"
+	  + "    <date variable=\"issued\" form=\"text\" date-parts=\"year\" prefix=\" \"/>"
+	  + "  </layout>"
+	  + "</citation>"
+	+ "</style>";
+
 var ITEM1 = {
 	"id": "ITEM-1",
 	"type": "book",
@@ -83,11 +94,27 @@ var ITEM2 = {
 			"given": "John"
 		},
 		{
-			"family": "Doe",
+			"family": "Roe",
 			"given": "Richard"
 		}
 	]
 };
+
+var ITEM3 = {
+	"id": "ITEM-3",
+	"type": "book",
+	"author": [
+		{
+			"family": "Wallbanger",
+			"given": "Harvey"
+		},
+		{
+			"family": "Smith",
+			"given": "Horatio"
+		}
+	]
+};
+
 
 
 var CITATION1 = {
@@ -116,6 +143,19 @@ var CITATION2 = {
 	}
 };
 
+var CITATION2x = {
+	"citationID": "CITATION-2",
+	"citationItems": [
+		{
+			"id": "ITEM-2"
+		}
+	],
+	"properties": {
+		"index": 0,
+		"noteIndex": 1
+	}
+};
+
 var CITATION3 = {
 	"citationID": "CITATION-3",
 	"citationItems": [
@@ -128,6 +168,34 @@ var CITATION3 = {
 		"noteIndex": 2
 	}
 };
+
+var CITATION4 = {
+	"citationID": "CITATION-4",
+	"citationItems": [
+		{
+			"id": "ITEM-3"
+		}
+	],
+	"properties": {
+		"index": 2,
+		"noteIndex": 3
+	}
+};
+
+var CITATION5 = {
+	"citationID": "CITATION-5",
+	"citationItems": [
+		{
+			"id": "ITEM-1"
+		}
+	],
+	"properties": {
+		"index": 1,
+		"noteIndex": 2
+	}
+};
+
+
 
 doh.register("citeproc_js.preview", [
 
@@ -170,8 +238,70 @@ doh.register("citeproc_js.preview", [
 		res2 = style.previewCitationCluster(CITATION2, [["CITATION-1", 1]], [], "html");
 		[data, res3] = style.processCitationCluster(CITATION3, [["CITATION-1", 1]], []);
 		doh.assertEqual("(Doe, Roe)", res1[0][1]);
-		doh.assertEqual("(J. Doe, R. Doe)", res2);
+		doh.assertEqual("(Doe, R. Roe)", res2);
 		doh.assertEqual("(Doe, Roe)", res3[0][1]);
 		doh.assertEqual(1, res3.length);
+	},
+	function testInitialsNeededOnlyWithOriginalCitationItemContent() {
+		var sys, style, res1, res2, res3, res4, res5, data;
+		sys = new RhinoTest([ITEM1, ITEM2, ITEM3]);
+		style =new CSL.Engine(sys,mycsl);
+		[data, res1] = style.processCitationCluster(CITATION1, [], []);
+		[data, res2] = style.processCitationCluster(CITATION2, [["CITATION-1", 1]], []);
+		// names rendered are:
+		//   C1i1: John Doe, Jane Roe
+		//   C2i2: John Doe, Richard Roe
+		//   C4i3: Harvey Wallbanger, Horatio Smith
+		res3 = style.processCitationCluster(CITATION4, [["CITATION-1", 1], ["CITATION-2", 2]], []);
+		// names rendered are:
+        //   C1i1: John Doe, Jane Roe
+		//   C5i1: John Doe, Jane Roe
+		//   C4i3: Harvey Wallbanger, Horatio Smith
+		res4 = style.previewCitationCluster(CITATION5, [["CITATION-1", 1]], [["CITATION-4", 3]], "html", CITATION2);
+		doh.assertEqual("(Doe, Roe)", res4);
+		// same as before preview:
+		//   C1i1: John Doe, Jane Roe
+		//   C2i2: John Doe, Richard Roe
+		//   C4i3: Harvey Wallbanger, Horatio Smith
+		[data, res5] = style.processCitationCluster(CITATION2, [["CITATION-1", 1]], [["CITATION-4", 3]]);
+		// This is the critical test in this fixture: if the processor
+		// state have been restored correctly following the preview,
+		// there will be no update to other citations.
+		doh.assertEqual(1, res5.length);
+		doh.assertEqual("(Doe, R. Roe)", res5[0][1]);
+	},
+	function testInitialsChangeWithExternalDelete() {
+		var sys, style, res1, res2, res3, res4, res5, data;
+		sys = new RhinoTest([ITEM1, ITEM2, ITEM3]);
+		style =new CSL.Engine(sys,mycsl);
+		[data, res1] = style.processCitationCluster(CITATION1, [], []);
+		// print("~~~~~~~~~~~~~~~~~~~~~~ print1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		[data, res2] = style.processCitationCluster(CITATION2, [["CITATION-1", 1]], []);
+		// names rendered are:
+		//   C1i1: John Doe, Jane Roe
+		//   C2i2: John Doe, Richard Roe
+		//   C4i3: Harvey Wallbanger, Horatio Smith
+		print("~~~~~~~~~~~~~~~~~~~~~~ print2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		res3 = style.processCitationCluster(CITATION4, [["CITATION-1", 1], ["CITATION-2", 2]], []);
+
+
+		// names rendered are:
+        //   C1i1: John Doe, Jane Roe
+		//   C5i1: John Doe, Jane Roe
+		//   C4i3: Harvey Wallbanger, Horatio Smith
+		// print("~~~~~~~~~~~~~~~~~~~~~~ preview ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		res4 = style.previewCitationCluster(CITATION5, [["CITATION-1", 1]], [["CITATION-4", 3]], "html", CITATION2);
+		doh.assertEqual("(Doe, Roe)", res4);
+
+
+
+		// changes to:
+		//   C2i2: John Doe, Richard Roe
+		//   C4i3: Harvey Wallbanger, Horatio Smith
+		// print("~~~~~~~~~~~~~~~~~~~~~~ print3 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		[data, res5] = style.processCitationCluster(CITATION2x, [], [["CITATION-4", 2]]);
+		//doh.assertEqual(1, res5);
+		doh.assertEqual("(Doe, Roe)", res5[0][1]);
+
 	}
 ]);
