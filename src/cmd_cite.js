becd48f3256ea92aebd0726fc2df55a977db794d
Frank Bennett <biercenator@gmail.com>
2010-06-16 10:15:23 +0900

    Avoid touching registry entries of items used in a citation to be previewed, that already exist in the registry.  Suppress redundant update call for citations that taint themselves in processCitationCluster().

--- src/cmd_cite.js
+++ src/cmd_cite.js
@@ -76,6 +76,7 @@ CSL.Engine.prototype.appendCitationCluster = function (citation) {
 
 CSL.Engine.prototype.processCitationCluster = function (citation, citationsPre, citationsPost, flag) {
 	var sortedItems, new_citation, pos, len, item, citationByIndex, c, Item, newitem, k, textCitations, noteCitations, update_items, citations, first_ref, last_ref, ipos, ilen, cpos, onecitation, oldvalue, ibidme, suprame, useme, items, i, key, prev_locator, curr_locator, param, ret, obj, ppos, llen, lllen, pppos, ppppos, llllen, cids, note_distance, return_data, lostItemId, lostItemList, lostItemData, otherLostPkeys;
+	this.debug = false;
 	return_data = {"bibchange": false};
 	this.registry.return_data = return_data;
 
@@ -99,8 +100,7 @@ CSL.Engine.prototype.processCitationCluster = function (citation, citationsPre, 
 		}
 		// Identify items that will go missing in the preview transaction
 		// Lots of action here, but faster than rerendering.
-		//var newCitationIds = citationsPre.concat([[citation.citationID, citation.properties.noteIndex]]).concat(citationsPost);
-		var newCitationIds = citationsPre.concat(citationsPost);
+		var newCitationIds = citationsPre.concat([[citation.citationID, citation.properties.noteIndex]]).concat(citationsPost);
 		var newItemIds = {};
 		for (pos = 0, len = newCitationIds.length; pos < len; pos += 1) {
 			c = this.registry.citationreg.citationById[newCitationIds[pos][0]];
@@ -566,6 +566,9 @@ CSL.Engine.prototype.processCitationCluster = function (citation, citationsPre, 
 		//
 		for (key in this.tmp.taintedCitationIDs) {
 			if (this.tmp.taintedCitationIDs.hasOwnProperty(key)) {
+				if (key === citation.citationID) {
+					continue;
+				}
 				obj = [];
 				citation = this.registry.citationreg.citationById[key];
 				obj.push(citation.properties.index);
