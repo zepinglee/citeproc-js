8009e2e59405c5d6075eb409b5a2b8e265d9065b
Frank Bennett <biercenator@gmail.com>
2010-07-15 04:54:39 +0900

    Fix typo in demo test source.

--- demo/loadcites.js
+++ demo/loadcites.js
@@ -1,9 +1,8 @@
 /*
- * Copyright (c) 2009 and 2010 Frank G. Bennett, Jr. All Rights
- * Reserved.
+ * Copyright (c) 2009 and 2010 Frank G. Bennett, Jr. All Rights Reserved.
  *
  * The contents of this file are subject to the Common Public
- * Attribution License Version 1.0 (the “License”); you may not use
+ * Attribution License Version 1.0 (the \u201cLicense\u201d); you may not use
  * this file except in compliance with the License. You may obtain a
  * copy of the License at:
  *
@@ -15,7 +14,7 @@
  * Original Developer. In addition, Exhibit A has been modified to be
  * consistent with Exhibit B.
  *
- * Software distributed under the License is distributed on an “AS IS”
+ * Software distributed under the License is distributed on an \u201cAS IS\u201d
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
  * the License for the specific language governing rights and limitations
  * under the License.
@@ -32,18 +31,6 @@
  * The Initial Developer of the Original Code is Frank G. Bennett,
  * Jr. All portions of the code written by Frank G. Bennett, Jr. are
  * Copyright (c) 2009 and 2010 Frank G. Bennett, Jr. All Rights Reserved.
- *
- * Alternatively, the contents of this file may be used under the
- * terms of the GNU Affero General Public License (the [AGPLv3]
- * License), in which case the provisions of [AGPLv3] License are
- * applicable instead of those above. If you wish to allow use of your
- * version of this file only under the terms of the [AGPLv3] License
- * and not to allow others to use your version of this file under the
- * CPAL, indicate your decision by deleting the provisions above and
- * replace them with the notice and other provisions required by the
- * [AGPLv3] License. If you do not delete the provisions above, a
- * recipient may use your version of this file under either the CPAL
- * or the [AGPLv3] License.”
  */
 
 var data = {
@@ -127,7 +114,7 @@ var data = {
 		"page": "744-750",
 		"volume":"76",
 		"issue": "3",
-		"doi":"10.1128/AEM.02396-09",
+		"DOI":"10.1128/AEM.02396-09",
 		"type": "article-journal"
 	},
 	"ITEM-4": {
