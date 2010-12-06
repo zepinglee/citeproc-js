65642f58b2ca438c72f6bfa2d0c4b05f90ec6517
Frank Bennett <biercenator@gmail.com>
2010-12-07 07:54:35 +0900

    Use localeConfigure() to set locale in build.js.

--- src/build.js
+++ src/build.js
@@ -48,7 +48,7 @@
 
 CSL.Engine = function (sys, style, lang, forceLang) {
 	var attrs, langspec, localexml, locale;
-	this.processor_version = "1.0.84";
+	this.processor_version = "1.0.85";
 	this.csl_version = "1.0";
 	this.sys = sys;
 	this.sys.xml = new CSL.System.Xml.Parsing();
@@ -186,23 +186,7 @@ CSL.Engine = function (sys, style, lang, forceLang) {
 	this.opt.lang = langspec.best;
 	this.opt["default-locale"][0] = langspec.best;
 	this.locale = {};
-	localexml = sys.xml.makeXml(sys.retrieveLocale("en-US"));
-	CSL.localeSet.call(this, sys, localexml, "en-US", langspec.best);
-	if (langspec.best !== "en-US") {
-		if (langspec.base !== langspec.best) {
-			localexml = sys.xml.makeXml(sys.retrieveLocale(langspec.base));
-			CSL.localeSet.call(this, sys, localexml, langspec.base, langspec.best);
-		}
-		localexml = sys.xml.makeXml(sys.retrieveLocale(langspec.best));
-		CSL.localeSet.call(this, sys, localexml, langspec.best, langspec.best);		
-	}
-
-	CSL.localeSet.call(this, sys, this.cslXml, "", langspec.best);
-	CSL.localeSet.call(this, sys, this.cslXml, langspec.bare, langspec.best);
-	if (langspec.base !== langspec.best) {
-		CSL.localeSet.call(this, sys, this.cslXml, langspec.base, langspec.best);
-	}
-	CSL.localeSet.call(this, sys, this.cslXml, langspec.best, langspec.best);
+	this.localeConfigure(langspec);
 
 	this.buildTokenLists("citation");
 	this.buildTokenLists("bibliography");
