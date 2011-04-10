b967dd64b41ef4e485700201e09acbae7a2e9865
Frank Bennett <biercenator@gmail.com>
2011-04-10 09:23:13 +0900

    Revision to node_name.js for new names handling.

--- src/node_name.js
+++ src/node_name.js
@@ -50,23 +50,10 @@ CSL.Node.name = {
 	build: function (state, target) {
 		var func, pos, len, attrname;
 		if ([CSL.SINGLETON, CSL.START].indexOf(this.tokentype) > -1) {
+			//state.fixOpt(this, "names-delimiter", "delimiter");
 			state.fixOpt(this, "name-delimiter", "delimiter");
 			state.fixOpt(this, "name-form", "form");
-			//
-			// Okay, there's a problem with these.  Each of these is set
-			// on the name object, but must be accessible at the closing of
-			// the enclosing names object.  How did I do this before?
-			//
-			// Boosting to tmp seems to be the current strategy, and although
-			// that's very messy, it does work.  It would be simple enough
-			// to extend the function applied to initialize-with below (which
-			// tests okay) to the others.  Probably that's the best short-term
-			// solution.
-			//
-			// The boost to tmp could be a boost to build, instead.  That would
-			// limit the jiggery-pokery and overhead to the compile phase.
-			// Might save a few trees, in aggregate.
-			//
+			
 			state.fixOpt(this, "and", "and");
 			state.fixOpt(this, "delimiter-precedes-last", "delimiter-precedes-last");
 			state.fixOpt(this, "delimiter-precedes-et-al", "delimiter-precedes-et-al");
@@ -80,52 +67,8 @@ CSL.Node.name = {
 			state.fixOpt(this, "et-al-subsequent-min", "et-al-subsequent-min");
 			state.fixOpt(this, "et-al-subsequent-use-first", "et-al-subsequent-use-first");
 
-			state.build.nameattrs = {};
-			len = CSL.NAME_ATTRIBUTES.length;
-			for (pos = 0; pos < len; pos += 1) {
-				attrname = CSL.NAME_ATTRIBUTES[pos];
-				state.build.nameattrs[attrname] = this.strings[attrname];
-			}
-
-			state.build.form = this.strings.form;
-			state.build.name_flag = true;
-
-			// set et al params
-			func = function (state, Item, item) {
-				if ("undefined" === typeof item) {
-					item = {};
-				}
-				if (item.position) {
-					if (! state.tmp["et-al-min"]) {
-						if (this.strings["et-al-subsequent-min"]) {
-							state.tmp["et-al-min"] = this.strings["et-al-subsequent-min"];
-						} else {
-							state.tmp["et-al-min"] = this.strings["et-al-min"];
-						}
-					}
-					if (! state.tmp["et-al-use-first"]) {
-						if (this.strings["et-al-subsequent-use-first"]) {
-							state.tmp["et-al-use-first"] = this.strings["et-al-subsequent-use-first"];
-						} else {
-							state.tmp["et-al-use-first"] = this.strings["et-al-use-first"];
-						}
-					}
-				} else {
-					if (! state.tmp["et-al-min"]) {
-						state.tmp["et-al-min"] = this.strings["et-al-min"];
-					}
-					if (! state.tmp["et-al-use-first"]) {
-						state.tmp["et-al-use-first"] = this.strings["et-al-use-first"];
-					}
-				}
-				if ("undefined" !== typeof this.strings["et-al-use-last"]) {
-					state.tmp["et-al-use-last"] = this.strings["et-al-use-last"];
-				}
-			};
-			this.execs.push(func);
-
 			func = function (state, Item) {
-				state.output.addToken("name", false, this);
+				state.nameOutput.name = this;
 			};
 			this.execs.push(func);
 		}
