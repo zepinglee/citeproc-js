cf7834afe1b6e5db12c20907baece0f0a4327c27
Frank Bennett <biercenator@gmail.com>
2011-04-10 14:20:23 +0900

    Yet more code.

--- src/util_names_join.js
+++ src/util_names_join.js
@@ -49,7 +49,7 @@ CSL.NameOutput.prototype._joinPersonsAll = function (blobs) {
 
 
 CSL.NameOutput.prototype._join = function (blobs, delimiter, single, multiple) {
-	//
+	// Eliminate false and empty blobs
 	for (var i = 0, ilen = blobs.length) {
 		if (!blobs[i] || !blobs[i].blobs.length) {
 			blobs = blobs.slice(0, i).concat(blobs.slice(i + 1));
@@ -57,18 +57,19 @@ CSL.NameOutput.prototype._join = function (blobs, delimiter, single, multiple) {
 	}
 	if (!blobs.length) {
 		return false;
-	} else if (blobs.length === 1) {
-		blobs = blobs[0];
 	} else if (single && blobs.length === 2) {
 		blobs = [blobs[0], single, blobs[1]];
 	} else {
 		if (multiple) {
-			for (var i = 0, ilen = blobs.length - 2; i < ilen; i += 1) {
-				blobs[i].strings.suffix += delimiter;
-			}
-			blobs = blobs.slice(0, -1).push(multiple).concat(blobs.slice(-1))
+			var delimiter_offset = 2;
 		} else {
-			blobs = blobs.slice();
+			var delimiter_offset = 1;
+		}
+		for (var i = 0, ilen = blobs.length - delimiter_offset; i < ilen; i += 1) {
+			blobs[i].strings.suffix += delimiter;
+		}
+		if (multiple) {
+			blobs = blobs.slice(0, -1).push(multiple).concat(blobs.slice(-1))
 		}
 	}
 	this.state.output.openLevel();
