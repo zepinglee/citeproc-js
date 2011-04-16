85da9e9c0a87f17e73aa7fc117e361f0b66c2ece
Frank Bennett <biercenator@gmail.com>
2011-04-16 17:29:42 +0900

    Names formatting: interim checkin of debugging trace.

--- src/util_names_disambig.js
+++ src/util_names_disambig.js
@@ -4,7 +4,9 @@ CSL.NameOutput.prototype.disambigNames = function () {
 	var pos = this.nameset_base;
 	for (var i = 0, ilen = this.variables.length; i < ilen; i += 1) {
 		var v = this.variables[i];
+	print("A: "+this.freeters[v]);
 		this._runDisambigNames(this.freeters[v], pos);
+	print("  B");
 		// We're skipping institutions, so +2
 		this.state.tmp.disambig_settings.givens.push([]);
 		pos += 2;
@@ -17,6 +19,7 @@ CSL.NameOutput.prototype.disambigNames = function () {
 
 CSL.NameOutput.prototype._runDisambigNames = function (lst, pos) {
 	for (var i = 0, ilen = lst.length; i < ilen; i += 1) {
+		print("NAME: "+i+" "+lst[i].family+" offset: "+this.nameset_offset);
 		//
 		// register the name in the global names disambiguation
 		// registry
@@ -25,7 +28,9 @@ CSL.NameOutput.prototype._runDisambigNames = function (lst, pos) {
 		if ("undefined" === typeof chk) {
 			this.state.tmp.disambig_settings.givens.push([]);
 		}
+		print("  yy: "+pos+" "+i+" // ("+this.state.tmp.disambig_settings.givens+")");
 		chk = this.state.tmp.disambig_settings.givens[pos][i];
+		print("  zz");
 		if ("undefined" === typeof chk) {
 			var myform = this.name.strings.form;
 			var myinitials = this.name.strings["initialize-with"];
