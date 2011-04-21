264de148d5ad8e56da47685596c2bafc82b26481
Frank Bennett <biercenator@gmail.com>
2011-04-22 00:09:20 +0900

    Names formatting: implement suppress-min attribute (non-CSL, specific to citeproc-js.

--- src/util_names_divide.js
+++ src/util_names_divide.js
@@ -57,6 +57,9 @@ CSL.NameOutput.prototype.divideAndTransliterateNames = function (Item, variables
 		var v = variables[i];
 		this.variable_offset[v] = this.nameset_offset;
 		var values = this._normalizeVariableValue(Item, v);
+		if (this.name.strings["suppress-min"] && values.length >= this.name.strings["suppress-min"]) {
+			values = [];
+		}
 		this._getFreeters(v, values);
 		this._getPersonsAndInstitutions(v, values);
 	}
