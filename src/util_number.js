49c1db61c3a6c5bb6d9498d777d3d6ce30037ee1
Frank Bennett <biercenator@gmail.com>
2011-05-15 17:12:27 +0900

    Adjustments to get the new alphabetic suffix function working.

--- src/util_number.js
+++ src/util_number.js
@@ -165,14 +165,15 @@ CSL.Util.Suffixator = function (slist) {
  * object with such a "format" method.</p>
  */
 
-CSL.Util.Suffixator.prototype.format = function (num) {
+CSL.Util.Suffixator.prototype.format = function (N) {
 	// Many thanks to Avram Lyon for this code, and good
 	// riddance to the several functions that it replaces.
 	var X, N;
+	N += 1;
 	var key = "";
 	do {
 		X = ((N % 26) == 0) ? 26 : (N % 26);
-		key = letters[X-1] + key;
+		key = this.slist[X-1] + key;
 		N = (N - X) / 26;
 	} while ( N != 0 );
 	return key;
