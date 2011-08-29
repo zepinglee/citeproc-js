47315f4fd9fef31d24dfbf4a879f95b31e785ab6
Frank Bennett <biercenator@gmail.com>
2011-08-29 02:08:59 +0000

    Recognize suppress-min='0' on individual cs:name or cs:institution nodes.

--- src/util_names_divide.js
+++ src/util_names_divide.js
@@ -68,6 +68,21 @@ CSL.NameOutput.prototype.divideAndTransliterateNames = function () {
 		}
 		this._getFreeters(v, values);
 		this._getPersonsAndInstitutions(v, values);
+		if (this.name.strings["suppress-min"] === 0) {
+			this.freeters[v] = [];
+			for (var j = 0, jlen = this.persons[v].length; j < jlen; j += 1) {
+				this.persons[v][j] = [];
+			}
+		} else if (this.institution.strings["suppress-min"] === 0) {
+			this.institutions[v] = [];
+			this.freeters[v] = this.freeters[v].concat(this.persons[v]);
+			for (var j = 0, jlen = this.persons[v].length; j < jlen; j += 1) {
+				for (var k = 0, klen = this.persons[v][j].length; k < klen; k += 1) {
+					this.freeters[v].push(this.persons[v][j][k])
+				}
+			}
+			this.persons[v] = [];
+		}
 	}
 };
 
