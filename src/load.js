12e6a9288b1b9f5dd02bf3eb8d9bd8518aaceb5f
Frank Bennett <biercenator@gmail.com>
2011-09-05 23:29:48 +0000

    Add event-place to the list of multilingual fields.

--- src/load.js
+++ src/load.js
@@ -131,7 +131,7 @@ var CSL = {
 
 	AREAS: ["citation", "citation_sort", "bibliography", "bibliography_sort"],
 
-	MULTI_FIELDS: ["publisher", "publisher-place", "title","container-title", "collection-title", "institution", "authority","edition"],
+	MULTI_FIELDS: ["publisher", "publisher-place", "event-place", "title","container-title", "collection-title", "institution", "authority","edition"],
 
 	CITE_FIELDS: ["first-reference-note-number", "locator"],
 
