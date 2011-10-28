0a012d6b70d50bac8764996a354cf279b9f41dc0
Frank Bennett <biercenator@gmail.com>
2011-10-28 11:57:02 +0000

    Update some abbreviation function calls in registry.js

--- src/registry.js
+++ src/registry.js
@@ -363,43 +363,45 @@ CSL.Registry.prototype.doinserts = function (mylist) {
             // If getAbbreviation is available, run it over any
             // relevant fields.
             if (this.state.sys.getAbbreviation) {
-                for (var field in this.state.transform.abbrevs) {
-                    switch (field) {
-                    case "place":
-                        if (Item["publisher-place"]) {
-                            this.state.sys.getAbbreviation(this.state.transform.abbrevs, field, Item["publisher-place"]);
-                        } else if (Item["event-place"]) {
-                            this.state.sys.getAbbreviation(this.state.transform.abbrevs, field, Item["event-place"]);
-                        }
-                        break;
+                for (var jurisdiction in this.state.transform.abbrevs) {
+                    for (var field in this.state.transform.abbrevs[jurisdiction]) {
+                        switch (field) {
+                        case "place":
+                            if (Item["publisher-place"]) {
+                                this.state.sys.getAbbreviation(this.state.transform.abbrevs, jurisdiction, field, Item["publisher-place"]);
+                            } else if (Item["event-place"]) {
+                                this.state.sys.getAbbreviation(this.state.transform.abbrevs, jurisdiction, field, Item["event-place"]);
+                            }
+                            break;
 
-                    case "institution":
-                        for (var creatorVar in CSL.CREATORS) {
-                            for (var creatorList in Item[creatorVar]) {
-                                for (j = 0, jlen = creatorList.length; j < jlen; j += 1) {
-                                    if (creatorList[j].isInstitution) {
-                                        var subOrganizations = creatorList[j].literal;
-                                        if (!subOrganizations) {
-                                            subOrganizations = creatorList[j].family;
-                                        }
-                                        if (subOrganizations) {
-                                            subOrganizations = subOrganizations.split(/\s*,\s*/);
-                                            for (k = 0, klen = subOrganizations.length; k < klen; k += 1) {
-                                                this.state.sys.getAbbreviation(this.state.transform.abbrevs, field, subOrganizations[k]);
+                        case "institution-part":
+                            for (var creatorVar in CSL.CREATORS) {
+                                for (var creatorList in Item[creatorVar]) {
+                                    for (j = 0, jlen = creatorList.length; j < jlen; j += 1) {
+                                        if (creatorList[j].isInstitution) {
+                                            var subOrganizations = creatorList[j].literal;
+                                            if (!subOrganizations) {
+                                                subOrganizations = creatorList[j].family;
+                                            }
+                                            if (subOrganizations) {
+                                                subOrganizations = subOrganizations.split(/\s*|\s*/);
+                                                for (k = 0, klen = subOrganizations.length; k < klen; k += 1) {
+                                                    this.state.sys.getAbbreviation(this.state.transform.abbrevs, jurisdiction, field, subOrganizations[k]);
+                                                }
                                             }
                                         }
                                     }
                                 }
                             }
-                        }
-                        break;
+                            break;
 
-                    default:
-                        if (Item[field]) {
-                            this.state.sys.getAbbreviation(this.state.transform.abbrevs, field, Item[field]);
-                        }
-                        break;
+                        default:
+                            if (Item[field]) {
+                                this.state.sys.getAbbreviation(this.state.transform.abbrevs, jurisdiction, field, Item[field]);
+                            }
+                            break;
 
+                        }
                     }
                 }
             }
