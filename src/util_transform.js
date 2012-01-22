42d38715883bbc9711f72eacd8c466725784b161
Frank Bennett <biercenator@gmail.com>
2012-01-22 10:45:45 +0000

    Map event to the "title" abbreviation list.

--- src/util_transform.js
+++ src/util_transform.js
@@ -135,7 +135,7 @@ CSL.Transform = function (state) {
             myabbrev_family = "institution-part";
         }
 
-        if (["genre"].indexOf(myabbrev_family) > -1) {
+        if (["genre", "event"].indexOf(myabbrev_family) > -1) {
             myabbrev_family = "title";
         }
 
