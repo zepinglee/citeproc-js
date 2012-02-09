83a562a5f049958fce355c09ecf4c78f0038206a
Frank Bennett <biercenator@gmail.com>
2012-02-09 03:32:16 +0000

    Bump version in build.js, update CHANGES.txt, bundle code.

--- src/build.js
+++ src/build.js
@@ -50,7 +50,7 @@
 
 CSL.Engine = function (sys, style, lang, forceLang) {
     var attrs, langspec, localexml, locale;
-    this.processor_version = "1.0.277";
+    this.processor_version = "1.0.278";
     this.csl_version = "1.0";
     this.sys = sys;
     this.sys.xml = new CSL.System.Xml.Parsing();
