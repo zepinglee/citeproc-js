b07deb097338ccd4a0a3fbad4ebe295ccc9acb00
Frank Bennett <biercenator@gmail.com>
2012-03-15 03:22:17 +0000

    Make jurisdiction a multi field, to wake up "abbreviation".

--- src/load.js
+++ src/load.js
@@ -177,7 +177,7 @@ var CSL = {
 
     AREAS: ["citation", "citation_sort", "bibliography", "bibliography_sort"],
 
-    MULTI_FIELDS: ["event", "publisher", "publisher-place", "event-place", "title", "container-title", "collection-title", "authority","edition","genre","title-short","subjurisdiction","medium"],
+    MULTI_FIELDS: ["event", "publisher", "publisher-place", "event-place", "title", "container-title", "collection-title", "authority","edition","genre","title-short","medium","jurisdiction"],
 
     CITE_FIELDS: ["first-reference-note-number", "locator", "locator-revision"],
 
