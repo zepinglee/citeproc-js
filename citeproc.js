a20993a01fa7f50788795eda3891563eb2c1bf83
Frank Bennett <biercenator@gmail.com>
2012-04-03 21:34:22 +0000

    Bundled code

--- citeproc.js
+++ citeproc.js
@@ -3983,7 +3983,11 @@ CSL.Engine.prototype.localeConfigure = function (langspec) {
     }
     this.localeSet(this.cslXml, langspec.best, langspec.best);
     if ("undefined" === typeof this.locale[langspec.best].terms["page-range-delimiter"]) {
-        this.locale[langspec.best].terms["page-range-delimiter"] = "\u2013";
+        if (["fr", "pt"].indexOf(langspec.best.slice(0, 2).toLowerCase()) > -1) {
+            this.locale[langspec.best].terms["page-range-delimiter"] = "-";
+        } else {
+            this.locale[langspec.best].terms["page-range-delimiter"] = "\u2013";
+        }
     }
 };
 CSL.Engine.prototype.localeSet = function (myxml, lang_in, lang_out) {
@@ -4066,9 +4070,6 @@ CSL.Engine.prototype.localeSet = function (myxml, lang_in, lang_out) {
                 }
             }
         }
-    }
-    if (lang_out && ["fr", "pt"].indexOf(lang_out.slice(0, 2).toLowerCase()) > -1) {
-        this.locale[lang_out].terms["page-range-delimiter"] = "-";
     }
     nodes = this.sys.xml.getNodesByName(locale, 'style-options');
     for (pos = 0, len = this.sys.xml.numberofnodes(nodes); pos < len; pos += 1) {
