9b80d01c5fc275e4db7a40b77bd45a7f1cba4f9d
Frank Bennett <biercenator@gmail.com>
2012-05-18 05:59:49 +0900

    Extend the tests for bib paging a bit

--- tests/citeproc-js/bibpaging.js
+++ tests/citeproc-js/bibpaging.js
@@ -118,6 +118,31 @@ doh.register("citeproc_js.bibpaging", [
 		}
 		var res = testme();
 		doh.assertEqual( true, res[0].done);
+	},
+
+	function testThree() {
+		function testme () {
+			try {
+			    var mysys = new RhinoTest();
+                var myids = [];
+                for (var i = 0, ilen = myitems.length; i < ilen; i += 1) {
+                    mysys._cache[myitems[i].id] = myitems[i];
+                    myids.push(myitems[i].id);
+                }
+				var citeproc = new CSL.Engine(mysys,mycsl);
+                citeproc.updateItems(myids);
+                var myparams = {page_start:"ITEM-1",page_length:2};
+                var ret = citeproc.makeBibliography(myparams);
+				return ret;
+			} catch (e) {
+				return e;
+			}
+		}
+		var res = testme();
+		doh.assertEqual( false, res[0].done);
+        doh.assertEqual( 2, res[1].length);
+        doh.assertEqual( "  <div class=\"csl-entry\">Item Two</div>\n", res[1][0]);
+        doh.assertEqual( "  <div class=\"csl-entry\">Item Three</div>\n", res[1][1]);
 	}
 ]);
 
