d6a9ffea194df4fc47e457d4c5f298bc88badea6
Frank Bennett <biercenator@gmail.com>
2012-06-16 19:26:08 +0900

    Use abbreviation fallback on secondary and tertiary forms

--- src/util_transform.js
+++ src/util_transform.js
@@ -352,11 +352,11 @@ CSL.Transform = function (state) {
 			secondary = false;
 			tertiary = false;
 			if (slot.secondary) {
-				res = getTextSubField(Item, variables[0], slot.secondary, false, res.usedOrig);
+				res = getTextSubField(Item, variables[0], slot.secondary, true, res.usedOrig);
                 secondary = res.name;
 			}
 			if (slot.tertiary) {
-				res = getTextSubField(Item, variables[0], slot.tertiary, false, res.usedOrig);
+				res = getTextSubField(Item, variables[0], slot.tertiary, true, res.usedOrig);
                 tertiary = res.name;
 			}
         
