15e0279e7eade6b4916524ecee21aa7c7b2c4249
Frank Bennett <biercenator@gmail.com>
2012-07-27 17:24:29 +0900

    Bump version in load.js, update CHANGES.txt, bundle code.

--- src/load.js
+++ src/load.js
@@ -87,7 +87,7 @@ if (!Array.indexOf) {
 
 var CSL = {
 
-    PROCESSOR_VERSION: "1.0.365",
+    PROCESSOR_VERSION: "1.0.366",
 
     STATUTE_SUBDIV_GROUPED_REGEX: /((?:^| )(?:art|ch|Ch|subch|p|pp|para|subpara|pt|r|sec|subsec|Sec|sch|tit)\.)/g,
     STATUTE_SUBDIV_PLAIN_REGEX: /(?:(?:^| )(?:art|ch|Ch|subch|p|pp|para|subpara|pt|r|sec|subsec|Sec|sch|tit)\.)/,
