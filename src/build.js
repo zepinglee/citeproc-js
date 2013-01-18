02807162718808d9a6be1c7e80ed41b102ef2756
Frank Bennett <biercenator@gmail.com>
2013-01-18 11:04:37 +0900

    Add missing piece of last checkin

--- src/build.js
+++ src/build.js
@@ -700,7 +700,11 @@ CSL.Engine.prototype.retrieveItem = function (id) {
     // Add getAbbreviation() call for title-short and container-title-short
     Item["title-short"] = Item.shortTitle;
     var isLegalType = ["legal_case","legislation","gazette","regulation"].indexOf(Item.type) > -1;
-    if (Item.title && this.sys.getAbbreviation && !isLegalType) {
+    if (isLegalType) {
+        if (!Item["title-short"]) {
+            Item["title-short"] = Item.title;
+        }
+    } else if (Item.title && this.sys.getAbbreviation) {
         var jurisdiction = this.transform.loadAbbreviation(Item.jurisdiction, "title", Item.title);
         if (this.transform.abbrevs[jurisdiction].title) {
             if (this.transform.abbrevs[jurisdiction].title[Item.title]) {
