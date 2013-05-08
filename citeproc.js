844641761a59fa7f0689bf3f6fb691e893fa4a0d
Frank Bennett <biercenator@gmail.com>
2013-05-08 09:09:16 +0900

    Bump version, update CHANGES.txt, bundle code

--- citeproc.js
+++ citeproc.js
@@ -57,7 +57,7 @@ if (!Array.indexOf) {
     };
 }
 var CSL = {
-    PROCESSOR_VERSION: "1.0.460",
+    PROCESSOR_VERSION: "1.0.461",
     CONDITION_LEVEL_TOP: 1,
     CONDITION_LEVEL_BOTTOM: 2,
     PLAIN_HYPHEN_REGEX: /(?:[^\\]-|\u2013)/,
