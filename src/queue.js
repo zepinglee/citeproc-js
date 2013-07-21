50231cef281aef23edd1202ce25b8cfe5da40291
Frank Bennett <biercenator@gmail.com>
2013-07-21 09:33:50 +0900

    Small change to accomodate collapsing of numeric blobs within a group

--- src/queue.js
+++ src/queue.js
@@ -509,7 +509,7 @@ CSL.Output.Queue.prototype.string = function (state, myblobs, blob) {
     if (blob && (blob.decorations.length || blob.strings.suffix || blob.strings.prefix)) {
         span_split = ret.length;
     }
-    var blobs_start = state.output.renderBlobs(ret.slice(0, span_split), blob_delimiter, true);
+    var blobs_start = state.output.renderBlobs(ret.slice(0, span_split), blob_delimiter, true, blob);
     if (blobs_start && blob && (blob.decorations.length || blob.strings.suffix || blob.strings.prefix)) {
         if (!state.tmp.suppress_decorations) {
             for (i = 0, ilen = blob.decorations.length; i < ilen; i += 1) {
@@ -583,7 +583,7 @@ CSL.Output.Queue.prototype.clearlevel = function () {
     }
 };
 
-CSL.Output.Queue.prototype.renderBlobs = function (blobs, delim, in_cite) {
+CSL.Output.Queue.prototype.renderBlobs = function (blobs, delim, in_cite, parent) {
     var state, ret, ret_last_char, use_delim, i, blob, pos, len, ppos, llen, pppos, lllen, res, str, params, txt_esc;
     txt_esc = CSL.getSafeEscape(this.state);
     if (!delim) {
@@ -594,6 +594,11 @@ CSL.Output.Queue.prototype.renderBlobs = function (blobs, delim, in_cite) {
     ret_last_char = [];
     use_delim = "";
     len = blobs.length;
+    if (len === 1 && typeof blobs[0] === "object" && parent) {
+        blobs[0].strings.prefix = parent.strings.prefix;
+        blobs[0].strings.suffix = parent.strings.suffix;
+        return blobs[0];
+    }
     var start = true;
     for (pos = 0; pos < len; pos += 1) {
         if (blobs[pos].checkNext) {
