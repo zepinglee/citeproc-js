3fdc454394974be464d1b21e1249cd2afb5b1ad6
Frank Bennett <biercenator@gmail.com>
2013-10-26 12:30:34 +0900

    Clean up sort function, which as definitely incorrect in many contexts

--- src/sort.js
+++ src/sort.js
@@ -48,59 +48,83 @@
 
 /*global CSL: true */
 
-CSL.getSortCompare = function () {
+CSL.getSortCompare = function (default_locale) {
     if (CSL.stringCompare) {
         return CSL.stringCompare;
     }
     var strcmp;
-    var sortCompare;
+    // In order, attempt the following:
+    //   (1) Set locale collation from processor language
+    //   (2) Use localeCompare()
     try {
         var localeService = Components.classes["@mozilla.org/intl/nslocaleservice;1"]
             .getService(Components.interfaces.nsILocaleService);
         var collationFactory = Components.classes["@mozilla.org/intl/collation-factory;1"]
             .getService(Components.interfaces.nsICollationFactory);
-        var collation = collationFactory.CreateCollation(localeService.getApplicationLocale());
+        var collation = collationFactory.CreateCollation(localeService.newLocale(default_locale));
         strcmp = function(a, b) {
             return collation.compareString(1, a, b);
         };
-        CSL.debug("Using collation sort");
+        CSL.debug("Using collation sort: "+default_locale);
     } catch (e) {
         CSL.debug("Using localeCompare sort");
         strcmp = function (a, b) {
             return a.toLocaleLowerCase().localeCompare(b.toLocaleLowerCase());
         };
     }
-    if (!strcmp("\u3044", "\u3046")) {
-        var isKana = /^[\u3040-\u309f\u30a0-\u30ff]/;
-        sortCompare = function (a, b) {
-            a = a.replace(/^[\[\]\'\"]*/, "").replace(/[\[\]\'\"]*$/, "");
-            b = b.replace(/^[\[\]\'\"]*/, "").replace(/[\[\]\'\"]*$/, "");
-            var ak = isKana.exec(a);
-            var bk = isKana.exec(b);
-            if (ak || bk) {
-                if (!ak) {
-                    return -1;
-                } else if (!bk) {
-                    return 1;
-                } else if (a < b) {
-                    return -1;
-                } else if (a > b) {
-                    return 1;
+    var isKana = /^[\[\]\'\"]*[\u3040-\u309f\u30a0-\u30ff]/;
+    var getKanaPreSort = function () {
+        if (strcmp("\u3044", "\u3046")) {
+            return false;
+        } else {
+            return function (a, b) {
+                a = a.replace(/^[\[\]\'\"]*/g, "");
+                b = b.replace(/^[\[\]\'\"]*/g, "");
+                var ak = isKana.exec(a);
+                var bk = isKana.exec(b);
+                if (ak || bk) {
+                    if (!ak) {
+                        return -1;
+                    } else if (!bk) {
+                        return 1;
+                    } else if (a < b) {
+                        return -1;
+                    } else if (a > b) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
                 } else {
-                    return 0;
+                    return false;
                 }
-            } else {
+            };
+        }
+    }
+    var getBracketPreSort = function () {
+        if (!strcmp("[x","x")) {
+            return false;
+        } else {
+            return function (a, b) {
+                a = a.replace(/^[\[\]\'\"]*/g, "");
+                b = b.replace(/^[\[\]\'\"]*/g, "");
                 return strcmp(a, b);
             }
-        };
-    } else if (strcmp("[x","x")) {
-        sortCompare = function (a, b) {
-            a = a.replace(/^[\[\]\'\"]*/, "").replace(/[\[\]\'\"]*$/, "");
-            b = b.replace(/^[\[\]\'\"]*/, "").replace(/[\[\]\'\"]*$/, "");
+        }
+    }
+    var kanaPreSort = getKanaPreSort();
+    var bracketPreSort = getBracketPreSort();
+    var sortCompare = function (a, b) {
+        if (kanaPreSort) {
+            var ret = kanaPreSort(a, b);
+            if (false !== ret) {
+                return ret;
+            }
+        }
+        if (bracketPreSort) {
+            return bracketPreSort(a, b);
+        } else {
             return strcmp(a, b);
         }
-    } else {
-        sortCompare = strcmp;
     }
     return sortCompare;
 };
