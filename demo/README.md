dfc93d5960e69b39ec35915999824189e6270386
Chris Maloney <voldrani@gmail.com>
2014-02-07 11:37:23 -0500

    Simplify demo

--- demo/README.md
+++ demo/README.md
@@ -4,35 +4,23 @@ The code in this folder can be used to spin up a local `citeproc-js`
 demo page.  It is mainly intended as a sample of running code for
 developers working with the processor.
 
-The demo requires `node.js`, so install that first. The page is
-provided by a tiny webserver that listens on port `8080` of the
-machine on which it is run. To start the server, clone this repo to
-your local machine, open a terminal, enter this `./demo` subdirectory,
-and run the following command:
+The demo.html page must be served from an HTTP server.  (Because it uses AJAX
+requests, it won't work if you open it in your browser from the filesystem.)
+So, either put this citeproc-js repository in a directory served by, for example,
+Apache, or you can use Node.js with the included tiny server script,
+node-server.js.
 
-    node ./tiny-node-server-for-demo.js
+To start the server, clone this repo to your local machine, open a terminal,
+enter this `./demo` subdirectory, and run the following command:
+
+    node ./node-server.js
 
 Point a browser at the address reported by the server, and the
 demo page should appear.
 
-The code is in four files, which are:
+See the demo.js file for more information about how it works.
 
-`citeproc_generic.js`
-: This contains the processor itself, bundled with the xmldom.js parser.
-
-`sys.js`
-: This contains a bare JavaScript object with two functions, `retrieveItem()` and `retrieveLocale()`. The former must return a valid CSL JSON object. The latter must return a valid *serialized* CSL locale (the processor will fail on a native XML object or buffer).
-
-`builder.js`
-: This defines a function used in the page to build processor instances, and places a dummy database object in memory for use by the `retrieveItem()` function contained in `sys.js`. Note that the CSL style code fed to the processor must, like the locale, be *serialized* XML.
-
-`runner.js`
-: This contains the JavaScript that runs the installed processor on the page.
-
-This particular demo runs `citeproc-js` in the browser client, but the processor
-can (of course) be run on the server side as well. It depends on your preferred
-tradeoff between snappiness and server burden.
-
+----
 Hope this helps. If you run into any snags, feel free to give me a shout.
 
 Frank Bennett
--- demo/demo.js
+++ demo/demo.js
@@ -3,7 +3,7 @@
  * Reserved.
  *
  * The contents of this file are subject to the Common Public
- * Attribution License Version 1.0 (the ‚ÄúLicense‚Äù); you may not use
+ * Attribution License Version 1.0 (the ìLicenseî); you may not use
  * this file except in compliance with the License. You may obtain a
  * copy of the License at:
  *
@@ -15,7 +15,7 @@
  * Original Developer. In addition, Exhibit A has been modified to be
  * consistent with Exhibit B.
  *
- * Software distributed under the License is distributed on an ‚ÄúAS IS‚Äù
+ * Software distributed under the License is distributed on an ìAS ISî
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
  * the License for the specific language governing rights and limitations
  * under the License.
@@ -43,17 +43,65 @@
  * replace them with the notice and other provisions required by the
  * [AGPLv3] License. If you do not delete the provisions above, a
  * recipient may use your version of this file under either the CPAL
- * or the [AGPLv3] License.‚Äù
+ * or the [AGPLv3] License.î
  */
 
+
+// Get the citations that we are supposed to render, in the CSL-json format
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'citations.json', false);
+xhr.send(null);
+var citations = JSON.parse(xhr.responseText);;
+
+
+// Initialize a system object, which contains two methods needed by the
+// engine.
 citeprocSys = {
+    // Given a language tag in RFC-4646 form, this method retrieves the
+    // locale definition file.  This method must return a valid *serialized*
+    // CSL locale. (In other words, an blob of XML as an unparsed string.  The
+    // processor will fail on a native XML object or buffer).
     retrieveLocale: function (lang){
         var xhr = new XMLHttpRequest();
         xhr.open('GET', 'locales-' + lang + '.xml', false);
         xhr.send(null);
         return xhr.responseText;
     },
-    retrieveItem: function (id){
-        return DATABASE[id];
+
+    // Given an identifier, this retrieves one citation item.  This method
+    // must return a valid CSL-JSON object.
+    retrieveItem: function(id){
+        return citations[id];
     }
 };
+
+// Given the identifier of a CSL style, this function instantiates a CSL.Engine
+// object that can render citations in that style.
+function getProcessor(styleID) {
+    // Get the CSL style as a serialized string of XML
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', styleID + '.csl', false);
+    xhr.send(null);
+    var styleAsText = xhr.responseText;
+
+    // Instantiate and return the engine
+    var citeproc = new CSL.Engine(citeprocSys, styleAsText);
+    return citeproc;
+};
+
+
+// This runs at document ready, and renders the bibliography
+function renderBib() {
+    var bibDivs = document.getElementsByClassName('bib-div');
+    for (var i = 0, ilen = bibDivs.length; i < ilen; ++i) {
+        var bibDiv = bibDivs[i];
+        var citeproc = getProcessor(bibDiv.getAttribute('data-csl'));
+        var itemIDs = [];
+        for (var key in citations) {
+            itemIDs.push(key);
+        }
+        citeproc.updateItems(itemIDs);
+        var bibResult = citeproc.makeBibliography();
+        bibDiv.innerHTML = bibResult[1].join('\n');
+    }
+}
