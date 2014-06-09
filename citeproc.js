702e15d7f5d4c0a61e6fcd42f73685d44144ea45
Frank Bennett <biercenator@gmail.com>
2014-06-09 11:41:27 +0900

    Bump version in load.js, update CHANGES.txt, bundle code

--- citeproc.js
+++ citeproc.js
@@ -10,7 +10,7 @@ if (!Array.indexOf) {
     };
 }
 var CSL = {
-    PROCESSOR_VERSION: "1.0.528",
+    PROCESSOR_VERSION: "1.0.529",
     CONDITION_LEVEL_TOP: 1,
     CONDITION_LEVEL_BOTTOM: 2,
     PLAIN_HYPHEN_REGEX: /(?:[^\\]-|\u2013)/,
@@ -1355,8 +1355,14 @@ CSL.Engine.prototype.buildTokenLists = function (area) {
 CSL.Engine.prototype.setStyleAttributes = function () {
     var dummy, attr, key, attributes, attrname;
     dummy = {};
-    dummy.name = this.sys.xml.nodename(this.cslXml);
-    attributes = this.sys.xml.attributes(this.cslXml);
+    var cslXml = this.cslXml;
+    if (!this.cslXml.tagName || ("" + this.cslXml.tagName).toLowerCase() !== 'style') {
+        if (this.cslXml.getElementsByTagName) {
+            var cslXml = this.cslXml.getElementsByTagName('style')[0];
+        }
+    }
+    dummy.name = this.sys.xml.nodename(cslXml);
+    attributes = this.sys.xml.attributes(cslXml);
     for (attrname in attributes) {
         if (attributes.hasOwnProperty(attrname)) {
             CSL.Attributes[attrname].call(dummy, this, attributes[attrname]);
@@ -1634,9 +1640,10 @@ CSL.Engine.prototype.retrieveItem = function (id) {
     if (this.opt.development_extensions.main_title_from_short_title) {
         Item["title-main"] = Item.title;
         Item["title-sub"] = false;
-        if (Item.title && Item.shortTitle) {
-            offset = Item.shortTitle.length;
-            if (Item.title.slice(0,offset) === Item.shortTitle && Item.title.slice(offset).match(/^\s*:/)) {
+        if (Item.title && (Item.shortTitle || Item['title-short'])) {
+            var shortTitle = Item.shortTitle ? Item.shortTitle : Item['title-short'];
+            offset = shortTitle.length;
+            if (Item.title.slice(0,offset) === shortTitle && Item.title.slice(offset).match(/^\s*:/)) {
                 Item["title-main"] = Item.title.slice(0,offset).replace(/\s+$/,"");
                 Item["title-sub"] = Item.title.slice(offset).replace(/^\s*:\s*/,"");
             }
--- src/load.js
+++ src/load.js
@@ -45,7 +45,7 @@ if (!Array.indexOf) {
 
 var CSL = {
 
-    PROCESSOR_VERSION: "1.0.528",
+    PROCESSOR_VERSION: "1.0.529",
 
     CONDITION_LEVEL_TOP: 1,
 
