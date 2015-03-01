d3005e9ae814e1f646e4eb77a329f9fb29aba7fd
Frank Bennett <biercenator@gmail.com>
2015-03-01 15:52:31 +0900

    Try multiple matches in particles parser

--- src/util_name_particles.js
+++ src/util_name_particles.js
@@ -1,77 +1,87 @@
 CSL.parseParticles = function(){
     var PARTICLES = [
-        ["abbé d'", [0,2], null],
-        ["al", [0,1], null],
-        ["dos", [0,1], null],
-        ["el", [0,1], null],
-        ["de las", [0,1], [1,2]],
-        ["lo", [0,1], null],
-        ["les", [0,1], null],
-        ["il", [0,1], null],
-        ["del", null, [0,1]],
-        ["dela", [0,1], null],
-        ["della", [0,1], null],
-        ["dello", [0,1], null],
-        ["di", null, [0,1]],
-        ["da", null, [0,1]],
-        ["do", null, [0,1]],
-        ["des", null, [0,1]],
-        ["lou", null, [0,1]],
-        ["pietro", null, [0,1]],
-        ["de", null, [0,1]],
-        ["de la", [0,1], [1,2]],
-        ["du", [0,1], null],
-        ["d'", [0,1], null],
-        ["le", null, [0,1]],
-        ["la", null, [0,1]],
-        ["l'", null, [0,1]],
-        ["saint", null, [0,1]],
-        ["sainte", null, [0,1]],
-        ["st.", null, [0,1]],
-        ["ste.", null, [0,1]],
-        ["van", null, [0,1]],
-        ["van de", null, [0,2]],
-        ["van der", null, [0,2]],
-        ["van den", null, [0,2]],
-        ["vander", null, [0,1]],
-        ["v.d.", null, [0,1]],
-        ["vd", null, [0,1]],
-        ["van het", null, [0,2]],
-        ["ver", null, [0,1]],
-        ["ten", [0,1], null],
-        ["ter", [0,1], null],
-        ["te", [0,1], null],
-        ["op de", [0,2], null],
-        ["in de", [0,2], null],
-        ["in 't", [0,2], null],
-        ["in het", [0,2], null],
-        ["uit de", [0,2], null],
-        ["uit den", [0,2], null],
-        ["von", [0,1], null],
-        ["von der", [0,2], null],
-        ["von dem", [0,2], null],
-        ["von zu", [0,2], null],
-        ["v.", [0,1], null],
-        ["v", [0,1], null],
-        ["vom", [0,1], null],
-        ["das", [0,1], null],
-        ["zum", [0,1], null],
-        ["zur", [0,1], null],
-        ["den", [0,1], null],
-        ["der", [0,1], null],
-        ["des", [0,1], null],
-        ["auf den", [0,2], null],
-        ["mac", null, [0,1]],
-        ["ben", null, [0,1]],
-        ["bin", null, [0,1]],
-        ["sen", [0,1], null],
-        ["al-", [0,1], null]
-    ]
+        ["abbé d'", [[[0,2], null]]],
+        ["al", [[[0,1], null]]],
+        ["al-", [[[0,1], null]],[[null,[0,1]]]],
+        ["auf den", [[[0,2], null]]],
+        ["ben", [[null, [0,1]]]],
+        ["bin", [[null, [0,1]]]],
+        ["d'", [[[0,1], null]],[[null,[0,1]]]],
+        ["da", [[null, [0,1]]]],
+        ["das", [[[0,1], null]]],
+        ["de", [[null, [0,1]],[[0,1],null]]],
+        ["de la", [[[0,1], [1,2]]]],
+        ["de las", [[[0,1], [1,2]]]],
+        ["del", [[null, [0,1]]]],
+        ["dela", [[[0,1], null]]],
+        ["della", [[[0,1], null]]],
+        ["dello", [[[0,1], null]]],
+        ["den", [[[0,1], null]]],
+        ["der", [[[0,1], null]]],
+        ["des", [[null, [0,1]],[[0,1], null]]],
+        ["di", [[null, [0,1]]]],
+        ["do", [[null, [0,1]]]],
+        ["dos", [[[0,1], null]]],
+        ["du", [[[0,1], null]]],
+        ["el", [[[0,1], null]]],
+        ["il", [[[0,1], null]]],
+        ["in 't", [[[0,2], null]]],
+        ["in de", [[[0,2], null]]],
+        ["in het", [[[0,2], null]]],
+        ["lo", [[[0,1], null]]],
+        ["les", [[[0,1], null]]],
+        ["l'", [[null, [0,1]]]],
+        ["la", [[null, [0,1]]]],
+        ["le", [[null, [0,1]]]],
+        ["lou", [[null, [0,1]]]],
+        ["mac", [[null, [0,1]]]],
+        ["op de", [[[0,2], null]]],
+        ["pietro", [[null, [0,1]]]],
+        ["saint", [[null, [0,1]]]],
+        ["sainte", [[null, [0,1]]]],
+        ["sen", [[[0,1], null]]],
+        ["st.", [[null, [0,1]]]],
+        ["ste.", [[null, [0,1]]]],
+        ["te", [[[0,1], null]]],
+        ["ten", [[[0,1], null]]],
+        ["ter", [[[0,1], null]]],
+        ["uit de", [[[0,2], null]]],
+        ["uit den", [[[0,2], null]]],
+        ["v.d.", [[null, [0,1]]]],
+        ["van", [[null, [0,1]]]],
+        ["van de", [[null, [0,2]]]],
+        ["van den", [[null, [0,2]]]],
+        ["van der", [[null, [0,2]]]],
+        ["van het", [[null, [0,2]]]],
+        ["vander", [[null, [0,1]]]],
+        ["vd", [[null, [0,1]]]],
+        ["ver", [[null, [0,1]]]],
+        ["von", [[[0,1], null]],[[null,[0,1]]]],
+        ["von der", [[[0,2], null]]],
+        ["von dem",[[[0,2], null]]],
+        ["von zu", [[[0,2], null]]],
+        ["v.", [[[0,1], null]]],
+        ["v", [[[0,1], null]]],
+        ["vom", [[[0,1], null]]],
+        ["zum", [[[0,1], null]]],
+        ["zur", [[[0,1], null]]]
+        ]
     var CATEGORIZER = null;
     function createCategorizer () {
         CATEGORIZER = {};
         for (var i=0,ilen=PARTICLES.length;i<ilen;i++) {
-            CATEGORIZER[PARTICLES[i][0]] = PARTICLES[i].slice(1);
+            var tLst = PARTICLES[i][0].split(" ");
+            var pInfo = [];
+            for (var j=0,jlen=PARTICLES[i][1].length;j<jlen;j++) {
+                var pParams = PARTICLES[i][1][j];
+                var str1 = pParams[0] ? tLst.slice(pParams[0][0], pParams[0][1]).join(" ") : "";
+                var str2 = pParams[1] ? tLst.slice(pParams[1][0], pParams[1][1]).join(" ") : "";
+                pInfo.push({
+                    strings: [str1, str2],
+                    positions: [pParams[0], pParams[1]]
+                });
+            }
+            CATEGORIZER[PARTICLES[i][0]] = pInfo;
         }
     }
     createCategorizer();
@@ -153,9 +163,10 @@ CSL.parseParticles = function(){
 
     function composeRegularExpressions () {
         composeParticleLists();
-        REX.family = new RegExp("^((?:" + LIST.family.space.join("|") + ")(\\s+)|(?:" + LIST.family.nospace.join("|") + "([^ ]))).*", "i");
+        REX.family = new RegExp("^((?:" + LIST.family.space.join("|") + ")(\\s+)|(?:" + LIST.family.nospace.join("|") + "([^\\s]))).*", "i");
         REX.given.full_comma = new RegExp(".*?(,[\\s]*)(" + LIST.given.full.join("|") + ")$", "i");
-        REX.given.full_lower = new RegExp(".*?([\\s]+)(" + LIST.given.full.join("|") + ")$");
+        REX.given.full_lower = new RegExp(".*?([ ]+)(" + LIST.given.full.join("|") + ")$");
+        X = "Tom du".match(REX.given.full_lower)
         var allInTheFamily = LIST.family.space
         for (var key in LIST.given.partial) {
             REX.given.partial[key] = new RegExp(".*?(\\s+)(" + LIST.given.partial[key].join("|") + ")$", "i");
@@ -173,30 +184,18 @@ CSL.parseParticles = function(){
             result.family.match = m[2] ? m[1] : m[3] ? m[1].slice(0,-m[3].length) : m[1];
             result.family.str = (m[2] ? m[1].slice(0,-m[2].length) : m[3] ? m[1].slice(0,-m[3].length) : m[1]);
             if (REX.given.partial[result.family.str.toLowerCase()]) {
-                // console.log("HIT ON TWO-PART: "+ name.family +", " + name.given);
                 var m = REX.given.partial[result.family.str.toLowerCase()].exec(name.given);
                 if (m) {
                     result.given.match = m[2] ? m[1] + m[2] : m[2];
                     result.given.str = m[2];
-                    // console.log(" " + result.given.str + " + " + result.family.str);
-                } else {
-                    // console.log(" " + result.family.str);
                 }
-            } else {
-                // console.log("HIT ON ONE-PART: "+ name.family + ", " + name.given);
-                // console.log(" " + result.family.str);
             }
         } else {
             var m = REX.given.full_comma.exec(name.given);
-            if (!m) REX.given.full_lower.exec(name.given);
+            if (!m) m = REX.given.full_lower.exec(name.given);
             if (m) {
                 result.given.match = m[1] ? m[1] + m[2] : m[2];
                 result.given.str = m[2];
-                // console.log("HIT ON GIVEN: "+ name.family + ", " + name.given);
-                // console.log(" "+result.given.match);
-                // console.log(" "+result.given.str);
-            } else {
-                // console.log("NO MATCH: "+name.family + ", " + name.given);
             }
         }
         return result;
@@ -230,15 +229,23 @@ CSL.parseParticles = function(){
         particles = particles.join(" ").split(" ");
         if (particles.length) {
             var key = particles.join(" ");
-            var types = CATEGORIZER[key.toLowerCase()];
-            if (types) {
-                if (types[0] !== null) {
-                    // Set element(s) as dropping-particle
-                    name["dropping-particle"] = particles.slice(types[0][0], types[0][1]).join(" ");
+            var pInfo = CATEGORIZER[key.toLowerCase()];
+            if (pInfo) {
+                for (var i=pInfo.length-1;i>-1;i--) {
+                    var pSet = pInfo[i];
+                    if (!result.family.str) result.family.str = "";
+                    if (!result.given.str) result.given.str = "";
+                    if (result.given.str === pSet.strings[0] && result.family.str === pSet.strings[1]) {
+                        break;
+                    }
                 }
-                if (types[1] !== null) {
+                if (pSet.positions[0] !== null) {
+                    // Set element(s) as dropping-particle
+                    name["dropping-particle"] = particles.slice(pSet.positions[0][0], pSet.positions[0][1]).join(" ");
+                }
+                if (pSet.positions[1] !== null) {
                     // Set element(s) as non-dropping-particle
-                    name["non-dropping-particle"] = particles.slice(types[1][0], types[1][1]).join(" ");
+                    name["non-dropping-particle"] = particles.slice(pSet.positions[1][0], pSet.positions[1][1]).join(" ");
                 }
             }
         }
