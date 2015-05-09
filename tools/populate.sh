578a6bca3121d65d2ab62ad0fd7fca9163f8f634
Frank Bennett <biercenator@gmail.com>
2015-05-09 22:38:58 +0900

    Send updates to the right projects

--- tools/populate.sh
+++ tools/populate.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 ./test.py -Z
-cp citeproc_zotero.js ~/src/zotero-processor/chrome/content/citeproc.js
+cp citeproc_zotero.js ~/src/propachi-vanilla/chrome/content/citeproc.js
 
 cat citeproc_zotero.js \
   | sed -s 's/this\.development_extensions\.main_title_from_short_title = false/this\.development_extensions\.main_title_from_short_title = true/' \
   | sed -s 's/this\.development_extensions\.uppercase_subtitles = false/this\.development_extensions\.uppercase_subtitles = true/' \
-  > ~/src/zotero-uppercase-subtitles/chrome/content/citeproc.js
+  > ~/src/propachi-upper/chrome/content/citeproc.js
 
-mv citeproc_zotero.js ~/src/zotero/chrome/content/zotero/xpcom/citeproc.js
+mv citeproc_zotero.js ~/src/jurism/chrome/content/zotero/xpcom/citeproc.js
+
+cp citeproc.js ~/src/juris-m-top/js/citeproc.js
