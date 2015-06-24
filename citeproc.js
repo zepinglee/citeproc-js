8ca79940c1afc7178196a6506a657d7b21863414
Frank Bennett <biercenator@gmail.com>
2015-06-25 00:32:21 +0900

    Bump version, update CHANGES, bundle

--- citeproc.js
+++ citeproc.js
@@ -10,7 +10,7 @@ if (!Array.indexOf) {
     };
 }
 var CSL = {
-    PROCESSOR_VERSION: "1.1.31",
+    PROCESSOR_VERSION: "1.1.32",
     CONDITION_LEVEL_TOP: 1,
     CONDITION_LEVEL_BOTTOM: 2,
     PLAIN_HYPHEN_REGEX: /(?:[^\\]-|\u2013)/,
@@ -7482,7 +7482,8 @@ CSL.NameOutput.prototype._renderInstitutionName = function (v, name, slot, j) {
             institution = [this._renderOneInstitutionPart(primary["short"], short_style)];
         } else {
             long_style = this._getLongStyle(primary, v, j);
-            institution = [this._renderOneInstitutionPart(primary["long"], long_style)];
+            institution_long = this._composeOneInstitutionPart([primary, secondary, tertiary], slot, long_style);
+            institution = [institution_long];
         }
         break;
     case "short-long":
--- src/load.js
+++ src/load.js
@@ -45,7 +45,7 @@ if (!Array.indexOf) {
 
 var CSL = {
 
-    PROCESSOR_VERSION: "1.1.31",
+    PROCESSOR_VERSION: "1.1.32",
 
     CONDITION_LEVEL_TOP: 1,
 
