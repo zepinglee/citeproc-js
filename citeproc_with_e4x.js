fab807a758577fa02264552333bb36ae314dff3c
Frank Bennett <biercenator@gmail.com>
2016-03-16 13:36:29 +0900

    Bump version number in bundled code. Oops.

--- citeproc_with_e4x.js
+++ citeproc_with_e4x.js
@@ -10,7 +10,7 @@ if (!Array.indexOf) {
     };
 }
 var CSL = {
-    PROCESSOR_VERSION: "1.1.80",
+    PROCESSOR_VERSION: "1.1.81",
     CONDITION_LEVEL_TOP: 1,
     CONDITION_LEVEL_BOTTOM: 2,
     PLAIN_HYPHEN_REGEX: /(?:[^\\]-|\u2013)/,
