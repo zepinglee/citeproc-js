07439265e07cea4735021680da5505c0ff10ccc3
fbennett <biercenator@gmail.com>
2016-03-25 07:18:00 +0900

    Enable RTL support in tests

--- src/test_runner.js
+++ src/test_runner.js
@@ -249,7 +249,7 @@ StdRhinoTest.prototype.run = function(){
     this.style.opt.development_extensions.static_statute_locator = true;
     this.style.opt.development_extensions.clobber_locator_if_no_statute_section = true;
     this.style.opt.development_extensions.handle_parallel_articles = true;
-    this.style.opt.development_extensions.rtl_support = false;
+    this.style.opt.development_extensions.rtl_support = true;
 	for (var opt in this.test.options) {
         if (opt === "variableWrapper") continue;
 		this.style.opt.development_extensions[opt] = this.test.options[opt];
