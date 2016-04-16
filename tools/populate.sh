817c7158e5890ee857a8b40c68fedda152842fb7
fbennett <biercenator@gmail.com>
2016-04-16 14:14:24 +0900

    Fix deployment script

--- tools/populate.sh
+++ tools/populate.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-./test.py -Z
-cp citeproc_zotero.js ~/src/propachi-vanilla/chrome/content/citeproc.js
+./test.py -B
+cp citeproc.js ~/src/propachi-vanilla/chrome/content/citeproc.js
 
-cat citeproc_zotero.js \
+cat citeproc.js \
   | sed -s 's/this\.development_extensions\.main_title_from_short_title = false/this\.development_extensions\.main_title_from_short_title = true/' \
   | sed -s 's/this\.development_extensions\.uppercase_subtitles = false/this\.development_extensions\.uppercase_subtitles = true/' \
   > ~/src/propachi-upper/chrome/content/citeproc.js
 
-mv citeproc_zotero.js ~/src/jurism/chrome/content/zotero/xpcom/citeproc.js
+cp citeproc.js ~/src/jurism/chrome/content/zotero/xpcom/citeproc.js
 
 cp citeproc.js ~/src/juris-m-top/js/citeproc.js
