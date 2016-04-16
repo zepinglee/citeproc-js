42740522105e0fc0c3ccbcf6ce7c3a008a270251
fbennett <biercenator@gmail.com>
2016-04-16 12:40:58 +0900

    Oops. Bump processor version in bundle

--- citeproc_with_e4x.js
+++ citeproc_with_e4x.js
@@ -34,7 +34,7 @@ if (!Array.indexOf) {
     };
 }
 var CSL = {
-    PROCESSOR_VERSION: "1.1.94",
+    PROCESSOR_VERSION: "1.1.95",
     CONDITION_LEVEL_TOP: 1,
     CONDITION_LEVEL_BOTTOM: 2,
     PLAIN_HYPHEN_REGEX: /(?:[^\\]-|\u2013)/,
