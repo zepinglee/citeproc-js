bb39960f11b6510f258f05654a7f0d86729d4852
fbennett <biercenator@gmail.com>
2016-04-16 14:15:35 +0900

    Fix script paths

--- tools/populate.sh
+++ tools/populate.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+set -e
+
 ./test.py -B
-cp citeproc.js ~/src/propachi-vanilla/chrome/content/citeproc.js
+cp citeproc.js ~/src/JM/propachi-vanilla/chrome/content/citeproc.js
 
 cat citeproc.js \
   | sed -s 's/this\.development_extensions\.main_title_from_short_title = false/this\.development_extensions\.main_title_from_short_title = true/' \
   | sed -s 's/this\.development_extensions\.uppercase_subtitles = false/this\.development_extensions\.uppercase_subtitles = true/' \
-  > ~/src/propachi-upper/chrome/content/citeproc.js
+  > ~/src/JM/propachi-upper/chrome/content/citeproc.js
 
-cp citeproc.js ~/src/jurism/chrome/content/zotero/xpcom/citeproc.js
+cp citeproc.js ~/src/JM/jurism/chrome/content/zotero/xpcom/citeproc.js
 
-cp citeproc.js ~/src/juris-m-top/js/citeproc.js
+cp citeproc.js ~/src/JM/juris-m-top/js/citeproc.js
