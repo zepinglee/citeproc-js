57aca0e031e4a4264e6f93c24c6523022a90f87d
fbennett <biercenator@gmail.com>
2016-07-02 10:07:34 +0900

    Use dump if console not available. Do not throw on CSL error

--- citeproc.js
+++ citeproc.js
@@ -781,17 +781,19 @@ if (typeof require !== "undefined" && typeof module !== 'undefined' && "exports"
 }
 CSL.TERMINAL_PUNCTUATION_REGEXP = new RegExp("^([" + CSL.TERMINAL_PUNCTUATION.slice(0, -1).join("") + "])(.*)");
 CSL.CLOSURES = new RegExp(".*[\\]\\)]");
-if ("object" === typeof console && "function" === typeof console.log) {
+if ("undefined" === typeof console) {
+    CSL.debug = function () {
+        dump("CSL: " + str + "\n");
+    };
+    CSL.error = function (str) {
+        dump("CSL error: " + str + "\n");
+    };
+} else {
     CSL.debug = function (str) {
         console.log("CSL: " + str);
     };
     CSL.error = function (str) {
         console.log("CSL error: " + str);
-    };
-} else {
-    CSL.debug = function () {};
-    CSL.error = function (str) {
-        throw "CSL error: " + str;
     };
 }
 CSL.XmlJSON = function (dataObj) {
--- src/print.js
+++ src/print.js
@@ -1,15 +1,17 @@
 /*global CSL: true */
 
-if ("object" === typeof console && "function" === typeof console.log) {
+if ("undefined" === typeof console) {
+    CSL.debug = function () {
+        dump("CSL: " + str + "\n");
+    };
+    CSL.error = function (str) {
+        dump("CSL error: " + str + "\n");
+    };
+} else {
     CSL.debug = function (str) {
         console.log("CSL: " + str);
     };
     CSL.error = function (str) {
         console.log("CSL error: " + str);
-    };
-} else {
-    CSL.debug = function () {};
-    CSL.error = function (str) {
-        throw "CSL error: " + str;
     };
 }
