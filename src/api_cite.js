57760f166f3a011c661726773831e99ac8bdd645
Derek Sifford <dereksifford@gmail.com>
2017-09-02 15:04:35 -0400

    Fix unsafe assignment

--- src/api_cite.js
+++ src/api_cite.js
@@ -198,6 +198,9 @@ CSL.Engine.prototype.processCitationCluster = function (citation, citationsPre, 
     }
     var update_items = [];
     for (var i = 0, ilen = citationByIndex.length; i < ilen; i += 1) {
+        if (!citationByIndex[i].properties) {
+            citationByIndex[i].properties = {};
+        }
         citationByIndex[i].properties.index = i;
         for (j = 0, jlen = citationByIndex[i].sortedItems.length; j < jlen; j += 1) {
             item = citationByIndex[i].sortedItems[j];
