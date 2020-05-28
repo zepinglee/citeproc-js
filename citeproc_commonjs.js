ee7b9528e13891c94c69b43574a5780c28fdc1cd
Frank Bennett <biercenator@gmail.com>
2020-05-28 17:05:59 +0900

    Bump version, update CHANGES, bundle as 1.3.18

--- citeproc_commonjs.js
+++ citeproc_commonjs.js
@@ -59,7 +59,7 @@ Copyright (c) 2009-2019 Frank Bennett
 
 var CSL = {
 
-    PROCESSOR_VERSION: "1.3.17",
+    PROCESSOR_VERSION: "1.3.18",
 
     error: function(str) { // default error function
         if ("undefined" === typeof Error) {
@@ -76,10 +76,10 @@ var CSL = {
         }
     },
 
-    LOCATOR_LABELS_REGEXP: new RegExp("^((vrs|sv|subpara|op|subch|add|amend|annot|app|art|bibliog|bk|ch|cl|col|cmt|dec|dept|div|ex|fig|fol|n|hypo|illus|intro|l|no|p|pp|para|pt|pmbl|princ|pub|r|sched|sec|ser|subdiv|subsec|supp|tbl|tit|vol)\\.)\\s+(.*)"),
+    LOCATOR_LABELS_REGEXP: new RegExp("^((vrs|sv|subpara|op|subch|add|amend|annot|app|art|bibliog|bk|ch|cl|col|cmt|dec|dept|div|ex|fig|fld|fol|n|hypo|illus|intro|l|no|p|pp|para|pt|pmbl|princ|pub|r|sched|sec|ser|subdiv|subsec|supp|tbl|tit|vol)\\.)\\s+(.*)"),
 
-    STATUTE_SUBDIV_PLAIN_REGEX: /(?:(?:^| )(?:vrs|sv|subpara|op|subch|add|amend|annot|app|art|bibliog|bk|ch|cl|col|cmt|dec|dept|div|ex|fig|fol|n|hypo|illus|intro|l|no|p|pp|para|pt|pmbl|princ|pub|r|sched|sec|ser|subdiv|subsec|supp|tbl|tit|vol)\. *)/,
-    STATUTE_SUBDIV_PLAIN_REGEX_FRONT: /(?:^\s*[.,;]*\s*(?:vrs|sv|subpara|op|subch|add|amend|annot|app|art|bibliog|bk|ch|cl|col|cmt|dec|dept|div|ex|fig|fol|n|hypo|illus|intro|l|no|p|pp|para|pt|pmbl|princ|pub|r|sched|sec|ser|subdiv|subsec|supp|tbl|tit|vol)\. *)/,
+    STATUTE_SUBDIV_PLAIN_REGEX: /(?:(?:^| )(?:vrs|sv|subpara|op|subch|add|amend|annot|app|art|bibliog|bk|ch|cl|col|cmt|dec|dept|div|ex|fig|fld|fol|n|hypo|illus|intro|l|no|p|pp|para|pt|pmbl|princ|pub|r|sched|sec|ser|subdiv|subsec|supp|tbl|tit|vol)\. *)/,
+    STATUTE_SUBDIV_PLAIN_REGEX_FRONT: /(?:^\s*[.,;]*\s*(?:vrs|sv|subpara|op|subch|add|amend|annot|app|art|bibliog|bk|ch|cl|col|cmt|dec|dept|div|ex|fig|fld|fol|n|hypo|illus|intro|l|no|p|pp|para|pt|pmbl|princ|pub|r|sched|sec|ser|subdiv|subsec|supp|tbl|tit|vol)\. *)/,
  
     STATUTE_SUBDIV_STRINGS: {
         "vrs.": "verse",
@@ -102,7 +102,8 @@ var CSL = {
         "dept.": "department",
         "div.": "division",
         "ex.": "example",
-        "fig.": "figure1",
+        "fig.": "figure",
+        "fld.": "field",
         "fol.": "folio",
         "n.": "note",
         "hypo.": "hypothetical",
@@ -151,6 +152,7 @@ var CSL = {
         "division": "div.",
         "example": "ex.",
         "figure": "fig.",
+        "field": "fld.",
         "folio": "fol.",
         "note": "n.",
         "hypothetical": "hypo.",
@@ -197,7 +199,8 @@ var CSL = {
         "dept": "department",
         "div": "division",
         "ex": "example",
-        "fig": "figure1",
+        "fig": "figure",
+        "fld": "field",
         "fol": "folio",
         "n": "note",
         "hypo": "hypothetical",
