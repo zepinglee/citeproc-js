1367b8187ce58b7bb87dbd96807889eedb19bcbb
Frank Bennett <biercenator@gmail.com>
2020-10-02 08:48:08 +0900

    Really fix grouping bug for comma-safe this time

--- src/node_group.js
+++ src/node_group.js
@@ -266,14 +266,12 @@ CSL.Node.group = {
             
             // quashnonfields
             func = function (state, Item, item) {
-                state.output.endTag();
-                if (!this.realGroup) {
-                    if (!state.tmp.group_context.tip.condition) {
-                        if (this.strings.suffix) {
-                            state.tmp.just_did_number = false;
-                        }
+                if (!state.tmp.group_context.tip.condition) {
+                    if (state.output.current.tip.strings.suffix) {
+                        state.tmp.just_did_number = false;
                     }
                 }
+                state.output.endTag();
                 if (this.realGroup) {
                     var flags = state.tmp.group_context.pop();
                     if (flags.parallel_delimiter_override) {
@@ -386,4 +384,3 @@ CSL.Node.group = {
         }
     }
 };
-
