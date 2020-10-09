d7db79ff8f4bdf16249239063a57c39bd1368c71
Frank Bennett <biercenator@gmail.com>
2020-10-09 20:34:46 +0900

    Bump version, update CHANGES, bundle as 1.4.38

--- src/load.js
+++ src/load.js
@@ -35,7 +35,7 @@
 
 var CSL = {
 
-    PROCESSOR_VERSION: "1.4.37",
+    PROCESSOR_VERSION: "1.4.38",
 
     error: function(str) { // default error function
         if ("undefined" === typeof Error) {
