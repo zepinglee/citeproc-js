0a8c9755e145656cd512c9c8676f68702270c74c
Frank Bennett <biercenator@gmail.com>
2021-01-06 08:47:53 +0900

    Limit c3a36ff5 to the page variable

--- src/util_number.js
+++ src/util_number.js
@@ -302,8 +302,10 @@ CSL.Engine.prototype.processNumber = function (node, ItemObject, variable) {
         str = normalizeFieldValue(str, defaultLabel);
 
         var jmrex, jsrex, mystr;
-        if (str.indexOf("\u2013") > -1) {
-            str = str.replace(/\u2013/g, "-");
+        if ("page" === variable) {
+            if (str.indexOf("\u2013") > -1) {
+                str = str.replace(/\u2013/g, "-");
+            }
         }
         if (str.indexOf("\\-") > -1) {
             jmrex = new RegExp(joinerMatchRex.source.replace("\\-", ""));
